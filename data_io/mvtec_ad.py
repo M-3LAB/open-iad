af4d7e9fc154fc4f45f8f3fe67d10b4e598ff459
xieguoyang <ronald.xie@foxmail.com>
2022-01-30 22:26:22 +0800

    update

--- data_io/mvtec_ad.py
+++ data_io/mvtec_ad.py
@@ -4,9 +4,29 @@ import os
 
 __all__ = ['MVTecDataset']
 
+def mvtec_classes():
+    return [
+        "bottle",
+        "cable",
+        "capsule",
+        "carpet",
+        "grid",
+        "hazelnut",
+        "leather",
+        "metal_nut",
+        "pill",
+        "screw",
+        "tile",
+        "toothbrush",
+        "transistor",
+        "wood",
+        "zipper",
+    ]
+
 class MVTec2D(Dataset):
-    def __init__(self):
-        pass
+    def __init__(self, data_path, mode='train', transform=None, mask_transform=None):
+        self.data_path = data_path
+        self.mode = mode
 
     def __getitem__(self):
         pass
