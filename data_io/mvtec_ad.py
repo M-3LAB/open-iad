9cd3a5dd7a97192fac754f32c22f715a8ddc7e84
xieguoyang <ronald.xie@foxmail.com>
2022-01-30 11:23:40 +0800

    update

--- data_io/mvtec_ad.py
+++ data_io/mvtec_ad.py
@@ -1,2 +1,12 @@
 import torch
-import torch.nn as nn
+from torch.utils.data import Dataset
+import os
+
+__all__ = ['MVTecDataset']
+
+class MVTec2D(Dataset):
+    def __init__(self):
+        pass
+
+    def __getitem__(self):
+        pass
