1a5945df3413938fb9816f92fe74aa59a9d1fbb6
xieguoyang <ronald.xie@foxmail.com>
2022-01-30 11:16:25 +0800

    update dataio base_class, Jinbao Not Finished Yet

---
+++ data_io/base_data.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+
+__all__ = ['BASE_DATA']
+
+#TODO: Jinbao Not Finihsed
+class BASE_DATA(torch.utils.data.Dataset):
+    def __init__(self) -> None:
+        super().__init__()
+        pass
+
+    def __getitem__(self):
+        pass
