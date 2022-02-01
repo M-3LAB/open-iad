f5d17aefc08fe4ae2bed23c319af05d9eea297b8
xieguoyang <ronald.xie@foxmail.com>
2022-02-01 09:33:53 +0800

    update

--- data_io/mvtec_ad.py
+++ data_io/mvtec_ad.py
@@ -2,7 +2,7 @@ import torch
 from torch.utils.data import Dataset
 import os
 
-__all__ = ['MVTecDataset']
+__all__ = ['MVTec2D', 'MVTec3D']
 
 def mvtec_classes():
     return [
@@ -24,9 +24,25 @@ def mvtec_classes():
     ]
 
 class MVTec2D(Dataset):
-    def __init__(self, data_path, mode='train', transform=None, mask_transform=None):
+    def __init__(self, data_path, class_name, mode='train', data_transform=None, 
+                 mask_transform=None):
+
         self.data_path = data_path
         self.mode = mode
+        self.data_transform = data_transform 
+        self.mask_transform = mask_transform
+        self.class_name = class_name
+
+        assert self.class_name in mvtec_classes
+
+
+    def __getitem__(self):
+        pass
+
+
+class MVTec3D(Dataset):
+    def __init__(self):
+        pass
 
     def __getitem__(self):
         pass
