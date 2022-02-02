c22c672e2dcc5ac97905bcc8bd4a730b09dda901
xieguoyang <ronald.xie@foxmail.com>
2022-02-02 13:03:06 +0800

    update

--- configuration/config.py
+++ configuration/config.py
@@ -1,3 +1,9 @@
 import argparse
 
 __all__ = ['parse_argument']
+
+def parse_argument():
+    parser = argparse.ArgumentParser()
+    parser.add_argument()
+    args = parser.parse_args()
+    return args 
