8cfb390d59274a82b24d8d603f1557b488b69d74
xieguoyang <ronald.xie@foxmail.com>
2022-02-02 13:12:39 +0800

    update

--- configuration/config.py
+++ configuration/config.py
@@ -4,6 +4,7 @@ __all__ = ['parse_argument']
 
 def parse_argument():
     parser = argparse.ArgumentParser()
-    parser.add_argument()
+    parser.add_argument('--mode', type=str, choices=['centralized', 'continual', 'federated'])
+    parser.add_argument('--phase', type=str, choices=['train', 'test'])
     args = parser.parse_args()
     return args 
