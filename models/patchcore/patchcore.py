99f1b40b716dd8396b1d0a4138319b5cffa583e8
xieguoyang <ronald.xie@foxmail.com>
2022-02-03 19:23:58 +0800

    update

--- models/patchcore/patchcore.py
+++ models/patchcore/patchcore.py
@@ -5,7 +5,7 @@ __all__ = ['PatchCore']
 
 class PatchCore(torch.nn.Module):
     def __init__(self):
-        super().__init__()
+        super(PatchCore).__init__()
     
     def fit(self, x):
         pass
