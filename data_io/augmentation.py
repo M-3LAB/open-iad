1bcb0f84bd3001f49ef62f0a2e7f78dc0bb8a1b8
xieguoyang <ronald.xie@foxmail.com>
2022-02-03 13:38:55 +0800

    update

--- data_io/augmentation.py
+++ data_io/augmentation.py
@@ -1,15 +1,15 @@
 from torchvision import transform as T
 
-__all__ =  ['standar_image_transform', 'standard_mask_transform']
+__all__ =  ['mvtec_image_transform', 'mvtec_mask_transform']
 
-standard_image_transform = T.Compose([T.Resize(224),
-                                      T.CenterCrop(224),
-                                      T.ToTensor(),
-                                      T.Normalize(mean=[0.485, 0.456, 0.406],
-                                                  std=[0.229, 0.224, 0.225])
-                                      ])
+mvtec_image_transform = T.Compose([T.Resize(224),
+                                   T.CenterCrop(224),
+                                   T.ToTensor(),
+                                   T.Normalize(mean=[0.485, 0.456, 0.406],
+                                               std=[0.229, 0.224, 0.225])
+                                   ])
 
-standard_mask_transform = T.Compose([T.Resize(224),
-                                     T.CenterCrop(224),
-                                     T.ToTensor()
-                                     ])
+mvtec_mask_transform = T.Compose([T.Resize(224),
+                                  T.CenterCrop(224),
+                                  T.ToTensor()
+                                  ])
