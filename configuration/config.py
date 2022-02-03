524788eb7d4f820144cfa1928656f5a92f31d588
xieguoyang <ronald.xie@foxmail.com>
2022-02-03 21:05:06 +0800

    update

--- configuration/config.py
+++ configuration/config.py
@@ -6,5 +6,6 @@ def parse_argument():
     parser = argparse.ArgumentParser()
     parser.add_argument('--mode', type=str, choices=['centralized', 'continual', 'federated'])
     parser.add_argument('--phase', type=str, choices=['train', 'test', 'ground_truth'])
+    parser.add_argument('--backbone-name', type=str, choices=['resnet18', 'wide_resnet50'])
     args = parser.parse_args()
     return args 
--- models/resnet_extractor.py
+++ models/resnet_extractor.py
@@ -1,11 +1,20 @@
 import torch
 import torch.nn as nn
+from torchvision import models
 
 __all__ = ['ResNetExtractor']
 
 class ResNetExtractor(nn.Module):
-    def __init__(self):
+    def __init__(self, device, backbone_name='resnet18'):
         super(ResNetExtractor).__init__()
+        assert backbone_name in ['resnet18', 'wide_resnet50']  
+
+        self.device = device
+
+        if backbone_name == 'resnet18':
+            self.backbone = models.resnet18(pretrained=True, progress=True).to(self.device)
+        elif backbone_name == 'wide_resnet50':
+            self.backbone = models.wide_resnet50_2(pretrained=True, progress=True).to(self.device)
     
     def forward(self, input):
         pass
