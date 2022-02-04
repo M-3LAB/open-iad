d71fd49c30363a787eabe51031b7cf8aea8a33c9
xieguoyang <ronald.xie@foxmail.com>
2022-02-04 09:25:44 +0800

    update

---
+++ metrics/auc_precision_recall.py
@@ -0,0 +1,11 @@
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_curve
+from sklearn.metrics import precision_recall_curve
+
+__all__ = ['get_auc', 'get_precision_recall']
+
+def get_auc():
+    pass
+
+def get_precision_recall():
+    pass
