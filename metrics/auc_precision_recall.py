8559fc9e1de7698a6e8e164bdcda2d43838ea1b7
xieguoyang <ronald.xie@foxmail.com>
2022-02-04 11:19:05 +0800

    update

--- metrics/auc_precision_recall.py
+++ metrics/auc_precision_recall.py
@@ -1,11 +1,23 @@
 from sklearn.metrics import roc_auc_score
 from sklearn.metrics import roc_curve
 from sklearn.metrics import precision_recall_curve
+import numpy as np
 
 __all__ = ['get_auc', 'get_precision_recall']
 
-def get_auc():
-    pass
+def get_auc(target, prediciton):
+    """
+    Args:
+        target: np.ndarray 
+        prediction: np.ndarray
+    """
+    score = roc_auc_score(target, prediciton)
+    return score
 
 def get_precision_recall():
+    """
+    Args:
+        target: np.ndarray 
+        prediction: np.ndarray
+    """
     pass
