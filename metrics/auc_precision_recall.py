bc23c862a2f11a60e263c5b84ee57a8684c68c4e
xieguoyang <ronald.xie@foxmail.com>
2022-02-04 11:24:11 +0800

    precision_recall finished

--- metrics/auc_precision_recall.py
+++ metrics/auc_precision_recall.py
@@ -14,10 +14,15 @@ def get_auc(target, prediciton):
     score = roc_auc_score(target, prediciton)
     return score
 
-def get_precision_recall():
+def get_precision_recall(target, prediction):
     """
     Args:
         target: np.ndarray 
         prediction: np.ndarray
     """
-    pass
+    precision, recall, thresholds = precision_recall_curve(target.flatten(), prediction.flatten())
+    a = 2 * precision * recall
+    b = precision + recall
+    f1 = np.divide(a, b, out=np.zeros_like(a), where=b != 0)
+    idx = np.argmax(f1)
+    return precision[idx], recall[idx], thresholds[idx]
