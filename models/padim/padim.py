d12633c04232f98c4c778ab36078013dc4a68006
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-02-16 09:26:12 +0800

    update

--- models/padim/padim.py
+++ models/padim/padim.py
@@ -1,3 +1,11 @@
 import torch
 import torch.nn as nn
 
+__all__ = ['PaDim']
+
+class PaDim(nn.Module):
+    def __init__(self):
+        super(PaDim).__init__()
+    
+    def forward(self, x):
+        pass
