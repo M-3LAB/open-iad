4525e84c3187416eb416ac67cd414a4902f5c945
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-02-16 09:56:29 +0800

    update

--- models/spade/spade.py
+++ models/spade/spade.py
@@ -1,3 +1,13 @@
+from turtle import forward
 import torch
 import torch.nn as nn
+
+__all__ = ['Spade']
+
+class Spade(nn.Module):
+    def __init__(self):
+        super(Spade).__init__()
+
+    def forward(self, x):
+        pass
 
