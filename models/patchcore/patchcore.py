3e4c74749b12b779f0203b6e1203c5d0ccdb7be7
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-02-17 16:13:09 +0800

    update

--- models/patchcore/patchcore.py
+++ models/patchcore/patchcore.py
@@ -1,12 +1,20 @@
 import torch
 import torch.nn as nn
 import numpy as np
+from models.resnet_extractor import ResNetExtractor 
 
 __all__ = ['PatchCore']
 
 class PatchCore(torch.nn.Module):
-    def __init__(self, backbone, device):
+    def __init__(self, backbone_name, device):
         super(PatchCore).__init__()
+
+        self.device = device
+        self.backbone_name = backbone_name
+        self.feat_extractor = ResNetExtractor(device=self.device, 
+                                              backbone_name=self.backbone_name) 
+        
+     
     
     def forward(self, x):
         pass
