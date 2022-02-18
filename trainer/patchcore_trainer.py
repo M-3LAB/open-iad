856a23afe397ca33f5dcd78a8fc9d9825e606915
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-02-18 11:41:54 +0800

    update

--- trainer/patchcore_trainer.py
+++ trainer/patchcore_trainer.py
@@ -2,15 +2,22 @@ import torch
 import torch.nn as nn
 from sklearn.random_projection import SaprseRandomProjection 
 from sklearn.neighbors import NearestNeighbors
+from sampling.kcenter_greedy import KCenterGreedy
 
 __all__ = ['PatchCoreTrainer']
 
 class PatchCoreTrainer(object):
 
     def __init__(self, config, train_loader, test_loader):
+
         self.config = config
         self.train_loader = train_loader
         self.test_loader = test_loader
+
+        self.random_projector = SaprseRandomProjection(n_components='auto',
+                                                       eps=0.9)
+        
+        self.selector = KCenterGreedy()
     
     def train(self):
         pass
