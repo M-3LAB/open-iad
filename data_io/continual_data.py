b298f46b1bfcb3edd31f89fba5f9c972ae54cba8
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-02-19 10:49:09 +0800

    update

--- data_io/continual_data.py
+++ data_io/continual_data.py
@@ -3,15 +3,22 @@ from data_io.mvtec_ad import *
 __all__ = ['CLData']
 class CLData(object):
     
-    def __init__(self, dataset, num_tasks):
+    def __init__(self, dataset, num_tasks=None):
         self.dataset = dataset
-        self.num_tasks = num_tasks
 
         self.data_list = []
         self.dataloader_list = []
 
+        if self.dataset == 'mvtec2d':
+            class_name = mvtec_2d_classes()
+        elif self.dataset == 'mvtec3d':
+            class_name = mvtec_3d_classes()
+
+        self.num_tasks = len(class_name) if num_tasks is None else num_tasks            
+        assert self.num_tasks <= len(class_name)
+
     def get_data(self):
         pass
-
+         
     def get_dataloader(self): 
         pass
