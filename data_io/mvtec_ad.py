ca5aa953d53a14509333b90eaf5a051b8d623583
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-02-19 09:33:42 +0800

    update

--- data_io/mvtec_ad.py
+++ data_io/mvtec_ad.py
@@ -119,7 +119,7 @@ class MVTec2D(Dataset):
     def __len__(self):
         return len(self.x)
 
-    def load_dataset_folder(self):
+    def load_dataset_folder(self, task_id=None):
         # input x, label y, [0, 1], good is 0 and bad is 1, mask is ground truth
         x, y, mask = [], [], []
         for sub_class_name in self.class_name:
@@ -156,96 +156,17 @@ class MVTec2D(Dataset):
                     mask.extend(gt_fpath_list)
 
         assert len(x) == len(y), 'number of x and y should be same'
-        return list(x), list(y), list(mask)
 
-class MVTec2DContinual(Dataset):#taskId is from 0 to floor(15/num_tasks_continual)
-    def __init__(self, data_path, class_name, taskId, mode='continual', phase='train', 
-                 data_transform=None, mask_transform=None):
+        if self.mode == 'continual':
+            assert task_id is not None
+            task_ids = len(x) * [task_id]
+            return list(x), list(y), list(mask), list(task_ids)
 
-        self.data_path = data_path
-        self.mode = mode
-        self.phase = phase
-        self.data_transform = data_transform 
-        self.mask_transform = mask_transform
-        self.class_name = class_name
-        assert set(self.class_name) <= set(mvtec_2d_classes())
-        # load dataset
-        self.x, self.y, self.mask, self.taskId = self.load_dataset_folder(taskId)
-
-        # data preprocessing 
-        self.data_transform = data_transform
-        self.mask_transform = mask_transform
-
-    def __getitem__(self, idx):
-        x, y, mask, taskId = self.x[idx], self.y[idx], self.mask[idx], self.taskId[idx]
-
-        x = Image.open(x).convert('RGB')
-        x = mvtec_2d_resize(x)
-
-        if y == 0:
-            mask = torch.zeros([x.size[0], x.size[1]])
+        elif self.mode == 'centralized':
+            return list(x), list(y), list(mask)
+        
         else:
-            mask = Image.open(mask)
-            # mask = self.mask_transforms(mask)
-
-        x = np.array(x)
-        mask = np.array(mask)
-
-        # check results
-        plt.subplot(121)
-        plt.imshow(x) 
-        plt.title('input')
-        plt.subplot(122)
-        plt.title('mask')
-        plt.imshow(mask) 
-        plt.savefig('./legacy_code/img_{}.jpg'.format('result'))
-        plt.close()
-
-        return x, y, mask, taskId
-
-    def __len__(self):
-        return len(self.x)
-
-    def load_dataset_folder(self, taskId):
-        # input x, label y, [0, 1], good is 0 and bad is 1, mask is ground truth
-        x, y, mask = [], [], []
-        for sub_class_name in self.class_name:
-            img_dir = os.path.join(self.data_path, sub_class_name, self.phase)
-            gt_dir = os.path.join(self.data_path, sub_class_name, 'ground_truth') 
-
-            img_types = sorted(os.listdir(img_dir))
-            for img_type in img_types:
-
-                # load images
-                img_type_dir = os.path.join(img_dir, img_type)
-                if not os.path.isdir(img_type_dir):
-                    continue
-                img_fpath_list = sorted([os.path.join(img_type_dir, f)
-                                        for f in os.listdir(img_type_dir)
-                                        if f.endswith('.png')])
-                x.extend(img_fpath_list)
-
-                """
-                Train Directory: Only Good Cases
-                Test Directory: Bad and Good Cases 
-                Ground Truth Directory: Only Bad Case
-                Detail Can be referred from MVTec 2D dataset
-                """
-                if img_type == 'good':
-                    y.extend([0] * len(img_fpath_list))
-                    mask.extend([None] * len(img_fpath_list))
-                else:
-                    y.extend([1] * len(img_fpath_list))
-                    gt_type_dir = os.path.join(gt_dir, img_type)
-                    img_fname_list = [os.path.splitext(os.path.basename(f))[0] for f in img_fpath_list]
-                    gt_fpath_list = [os.path.join(gt_type_dir, img_fname + '_mask.png')
-                                    for img_fname in img_fname_list]
-                    mask.extend(gt_fpath_list)
-
-        assert len(x) == len(y), 'number of x and y should be same'
-        tasksId = len(x)*[taskId] 
-        return list(x), list(y), list(mask), list(tasksId)
-
+            raise NotImplementedError('This mode has not implemented yet')
 
 class MVTec3D(Dataset):
     def __init__(self, data_path, class_name):
