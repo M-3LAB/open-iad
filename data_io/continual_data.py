7a4623e95ad610c1e64c56805ffdb7d2302e16ef
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-02-19 11:56:34 +0800

    update

--- data_io/continual_data.py
+++ data_io/continual_data.py
@@ -1,26 +1,34 @@
 from data_io.mvtec_ad import *
+from torch.utils.data import DataLoader
 
 __all__ = ['CLData']
 class CLData(object):
     
-    def __init__(self, dataset, num_tasks=None):
-        self.dataset = dataset
+    def __init__(self, config, dataset_name, num_tasks=None):
+
+        self.config = config
+
+        self.dataset_name = dataset_name
 
         self.data_list = []
         self.dataloader_list = []
 
-        if self.dataset == 'mvtec2d':
+        if self.dataset_name == 'mvtec2d':
             self.class_name = mvtec_2d_classes()
-        elif self.dataset == 'mvtec3d':
+        elif self.dataset_name == 'mvtec3d':
             self.class_name = mvtec_3d_classes()
         else:
             raise NotImplementedError('Not Implemented Yet')
+        
+        self.num_classes = len(self.class_name)
 
-        self.num_tasks = len(self.class_name) if num_tasks is None else num_tasks            
+        self.num_tasks = self.num_classes if num_tasks is None else num_tasks            
         assert self.num_tasks <= len(self.class_name)
 
     def get_data(self):
+        #for i in range():
         pass
          
     def get_dataloader(self): 
-        pass
+        for j in self.dataset_list:
+            train_loader = DataLoader()
