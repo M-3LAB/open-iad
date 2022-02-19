3726e21da0649c40bf933e48a7c61ab446128a3d
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-02-19 10:52:29 +0800

    update

--- data_io/continual_data.py
+++ data_io/continual_data.py
@@ -10,12 +10,14 @@ class CLData(object):
         self.dataloader_list = []
 
         if self.dataset == 'mvtec2d':
-            class_name = mvtec_2d_classes()
+            self.class_name = mvtec_2d_classes()
         elif self.dataset == 'mvtec3d':
-            class_name = mvtec_3d_classes()
+            self.class_name = mvtec_3d_classes()
+        else:
+            raise NotImplementedError('Not Implemented Yet')
 
-        self.num_tasks = len(class_name) if num_tasks is None else num_tasks            
-        assert self.num_tasks <= len(class_name)
+        self.num_tasks = len(self.class_name) if num_tasks is None else num_tasks            
+        assert self.num_tasks <= len(self.class_name)
 
     def get_data(self):
         pass
