786550417ecb3c28bbd51f27e88e850f78801d36
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-04-21 14:09:13 +0800

    update

--- metrics/chamfer_distance/chamfer_distance.py
+++ metrics/chamfer_distance/chamfer_distance.py
@@ -1,3 +1,7 @@
 import torch
 import torch.nn as nn
 
+__all__ = ['calculate_chamfer_distance']
+
+def calculate_chamfer_distance():
+    pass
---
+++ metrics/density_chamfer_distance/density_chamfer_distance.py
@@ -0,0 +1,8 @@
+import torch
+import torch.nn as nn
+
+
+__all__ = ['calculate_density_chamfer_distance']
+
+def calculate_density_chamfer_distance():
+    pass
