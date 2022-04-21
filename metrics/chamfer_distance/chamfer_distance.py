ccbaf59a91b8004f20a99751c838c6ee2492fcf3
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-04-21 14:55:02 +0800

    update

--- metrics/chamfer_distance/chamfer_distance.py
+++ metrics/chamfer_distance/chamfer_distance.py
@@ -3,5 +3,11 @@ import torch.nn as nn
 
 __all__ = ['calculate_chamfer_distance']
 
-def calculate_chamfer_distance():
+def calculate_chamfer_distance(x, gt):
+    """
+
+    Args:
+        x (torch tensor): input point cloud tensor
+        gt (torch tensor): groundtruth point cloud tensor 
+    """
     pass
