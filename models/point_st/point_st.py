49e783d3d67665bb43f790f30f74ab26dbbabe87
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-04-22 10:53:01 +0800

    update

--- models/point_st/point_st.py
+++ models/point_st/point_st.py
@@ -3,3 +3,12 @@ import torch.nn as nn
 
 __all__ = ['PointST']
 
+class PointST(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def forward(self, x):
+        pass
+
