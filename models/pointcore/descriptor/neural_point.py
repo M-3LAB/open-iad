794f1d059c987a203ae69bef92877cede9239399
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-04-22 10:11:14 +0800

    update

---
+++ models/pointcore/descriptor/neural_point.py
@@ -0,0 +1,13 @@
+from turtle import forward
+import torch
+import torch.nn as nn
+
+__all__ = ['NeuralPoint']
+
+class NeuralPoint(nn.Module):
+    def __init__(self):
+        super(NeuralPoint, self).__init__()
+        pass
+
+    def forward(self, x):
+        pass
---
+++ models/pointcore/pointcore.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+
+__all__ = ['PointCore']
+
+class PointCore(nn.Module):
+
+    def __init__(self):
+        super(PointCore).__init__()
+        pass
+
+    def forward(self, x):
+        pass
