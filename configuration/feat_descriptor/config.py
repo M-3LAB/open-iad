a43a91336e8fad8a66721b49a52f8e866bf0a65d
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-04-24 18:28:03 +0800

    update

--- configuration/feat_descriptor/config.py
+++ configuration/feat_descriptor/config.py
@@ -4,6 +4,7 @@ __all__ = ['parse_arguments_feat_descriptor']
 
 def parser_arguments_feat_descriptor():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dataset', '-d', type=str, default='mvtec3d', choices=['mvtec3d', 'model40'])
+    parser.add_argument('--dataset', '-d', type=str, default=None, choices=['mvtec3d', 'model40'])
+    parser.add_argument('--feat', '-f', type=str, default=None, choices=['neuralpoint_dgcnn', 'neuralpoint_pointmlp'])
     args = parser.parse_args()
     return args
--- tools/utilize.py
+++ tools/utilize.py
@@ -117,19 +117,6 @@ def weights_init_normal(m):
         torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
         torch.nn.init.constant_(m.bias.data, 0.0)
 
-
-class LambdaLR:
-    def __init__(self, n_epochs, offset, decay_start_epoch):
-        assert (
-            n_epochs - decay_start_epoch) > 0, "Decay must start before the training session ends!"
-        self.n_epochs = n_epochs
-        self.offset = offset
-        self.decay_start_epoch = decay_start_epoch
-
-    def step(self, epoch):
-        return 1.0 - max(0, epoch + self.offset - self.decay_start_epoch) / (self.n_epochs - self.decay_start_epoch)
-
-
 def record_path(para_dict):
     # mkdir ./work_dir/fed/brats/time-dir
     localtime = time.asctime(time.localtime(time.time()))
