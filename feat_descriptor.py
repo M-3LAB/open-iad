8d601dc1ba032f86d3e764a0ecac2ae8c8394729
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-04-24 18:51:00 +0800

    update

--- feat_descriptor.py
+++ feat_descriptor.py
@@ -2,7 +2,13 @@ import torch
 import torch.nn as nn
 from configuration.feat_descriptor.config import parser_arguments_feat_descriptor
 from tools.utilize import *
+import yaml
 
 if __name__ == '__main__':
     args = parser_arguments_feat_descriptor()
-    pass
+
+    with open('./configuration/kaid/{}.yaml'.format(args.dataset), 'r') as f:
+        para_dict = yaml.load(f, Loader=yaml.SafeLoader)
+
+    para_dict = merge_config(para_dict, args)
+    print(para_dict)
