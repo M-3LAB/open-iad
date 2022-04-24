51aa2383d4020c5dc3db47d26c8836d9e844dea5
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-04-24 18:42:52 +0800

    add feature descriptor

--- feat_descriptor.py
+++ feat_descriptor.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from configuration.feat_descriptor.config import parser_arguments_feat_descriptor
+from tools.utilize import *
 
 if __name__ == '__main__':
     args = parser_arguments_feat_descriptor()
