7a5446d2427696ef69c9d22e9c7b8fe3f3c33443
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-04-25 10:57:47 +0800

    update

--- models/pointcore/descriptor/point_mlp/point_mlp.py
+++ models/pointcore/descriptor/point_mlp/point_mlp.py
@@ -2,3 +2,11 @@ import torch
 import torch.nn as nn
 
 __all__ = ['PointMLP']
+
+class PointMLP(nn.Module):
+    def __init__(self, num_affinity_points):
+        super(PointMLP).__init__()
+        self.num_affinity_points = num_affinity_points
+
+    def forward(self, x):
+        pass
