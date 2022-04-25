f4546a52ce7d832f40762aabb543cb7cb03c6f41
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-04-25 11:16:42 +0800

    update

--- models/pointcore/descriptor/point_mlp/modules/activation.py
+++ models/pointcore/descriptor/point_mlp/modules/activation.py
@@ -1,2 +1,21 @@
 import torch
 import torch.nn as nn
+
+
+__all__ = ['get_activation']
+
+def get_activation(activation):
+    if activation.lower() == 'gelu':
+        return nn.GELU()
+    elif activation.lower() == 'rrelu':
+        return nn.RReLU(inplace=True)
+    elif activation.lower() == 'selu':
+        return nn.SELU(inplace=True)
+    elif activation.lower() == 'silu':
+        return nn.SiLU(inplace=True)
+    elif activation.lower() == 'hardswish':
+        return nn.Hardswish(inplace=True)
+    elif activation.lower() == 'leakyrelu':
+        return nn.LeakyReLU(inplace=True)
+    else:
+        return nn.ReLU(inplace=True)
