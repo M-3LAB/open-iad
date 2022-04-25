662dafa521dddbb528b9f41deea68b2f7b954b51
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-04-25 10:35:34 +0800

    update

--- metrics/chamfer_distance/chamfer_distance.py
+++ metrics/chamfer_distance/chamfer_distance.py
@@ -5,7 +5,6 @@ __all__ = ['calculate_chamfer_distance']
 
 def calculate_chamfer_distance(x, gt):
     """
-
     Args:
         x (torch tensor): input point cloud tensor
         gt (torch tensor): groundtruth point cloud tensor 
