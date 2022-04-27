7f6c45d67ff99c481bc8944a74b63a0c5d9cc4a3
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-04-27 13:46:56 +0800

    update

--- configuration/mmad/config.py
+++ configuration/mmad/config.py
@@ -1,1 +1,16 @@
 import argparse
+
+__all__ = ['parse_arguments_mmad']
+
+def parse_arguments_mmad():
+    parser = argparse.ArgumentParser() 
+    parser.add_argument('--object-id', default=None, type=int)
+    parser.add_argument('--batch-size', default=None, type=int)
+    parser.add_argument('--lr', default=None, type=float)
+    parser.add_argument('--gpu-id', default=None, type=int)
+    parser.add_argument('--num-epochs', default=None, type=int)
+    parser.add_argument('--data-path', default=None, type=str)
+    parser.add_argument('--dataset', default=None, type=str)
+    
+    args = parser.parse_args()
+    return args
