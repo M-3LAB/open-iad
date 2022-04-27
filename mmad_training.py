db7d7ed05da0892527329db50ee7a12aa7ff9491
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-04-27 16:08:30 +0800

    update

--- mmad_training.py
+++ mmad_training.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import yaml
 from configuration.mmad.config import parse_arguments_mmad 
 from tools.utilize import *
+from data_io.mvtec3d import MVTec3D
 
 if __name__ == '__main__':
 
@@ -14,7 +15,27 @@ if __name__ == '__main__':
     para_dict = merge_config(para_dict, args)
     print(para_dict)
 
-    #TODO: Data Loader
+    seed_everything(para_dict['seed'])
+
+    device, device_ids = parse_device_list(para_dict['gpu_ids'], int(para_dict['gpu_id']))
+    device = torch.device("cuda", device)
+
+    mvtec3d_transform = {'data_size':para_dict['data_size'],
+                         'mask_size':para_dict['mask_size']}
+    
+    if para_dict['dataset'] == 'mvtec3d':
+
+        train_dataset = MVTec3D(data_path=para_dict['data_path'],
+                                         learning_mode=self.para_dict['learning_mode'],
+                                         phase='train',
+                                         data_transform=mvtec3d_transform,
+                                         num_task=para_dict['num_task'])
+
+        valid_dataset = MVTec3D(data_path=para_dict['data_path'],
+                                learning_mode=para_dict['learning_mode'],
+                                phase='test',
+                                data_transform=mvtec3d_transform)
+
 
     #TODO: Model 
 
