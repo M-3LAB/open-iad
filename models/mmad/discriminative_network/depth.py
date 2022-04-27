1192f94827f2127337a6da17c23ea26184922cec
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-04-27 10:25:46 +0800

    update

---
+++ models/mmad/discriminative_network/depth.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+
+__all__ = ['DepthDis']
+
+class DepthDis(nn.Module):
+    def __init__(self):
+        super(DepthDis).__init__()
+        pass
+
+    def forward(self, x):
+        pass
---
+++ models/mmad/reconstruction_network/depth.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+
+__all__ = ['DepthRecons']
+
+class DepthRecons(nn.Module):
+    def __init__(self):
+        super(DepthRecons).__init__()
+        pass
+
+    def forward(self, x):
+        pass
