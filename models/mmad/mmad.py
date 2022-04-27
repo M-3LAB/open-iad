47c5f7db2c6bbd5f6199f113c9f5fec14e70c239
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-04-27 09:49:23 +0800

    update

--- models/mmad/mmad.py
+++ models/mmad/mmad.py
@@ -1,2 +1,13 @@
 import torch
 import torch.nn as nn
+
+__all__ = ['MMAD']
+
+class MMAD(nn.Module):
+
+    def __init__(self):
+        super(MMAD, self).__init__()
+        pass
+
+    def forward(self, x):
+        pass
