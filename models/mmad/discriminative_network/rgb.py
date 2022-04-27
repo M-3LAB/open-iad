818bc926b81e4599cc3c19bb44da1273ab2faa6e
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-04-27 09:40:27 +0800

    update

---
+++ models/mmad/discriminative_network/rgb.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+
+__all__=['RGBDis']
+
+class RGBDis(nn.Module):
+    def __init__(self):
+        super(RGBDis, self).__init__()
+        pass
+
+    def forward(self, x):
+        pass
---
+++ models/mmad/reconstruction_network/rgb.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+
+__all__ = ['RGBRecons']
+
+class RGBRecons(nn.Module):
+
+    def __init__(self):
+        super(RGBRecons, self).__init__()
+        pass 
+
+    def forward(self, x):
+        pass
