6b305be28c12666fdbe5390903d27f189d967d82
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-04-27 15:18:10 +0800

    update

--- feat_descriptor.py
+++ feat_descriptor.py
@@ -9,7 +9,7 @@ from models.pointcore.descriptor.neural_point.neural_point import NeuralPoint
 if __name__ == '__main__':
     args = parser_arguments_feat_descriptor()
 
-    with open('./configuration/kaid/{}.yaml'.format(args.dataset), 'r') as f:
+    with open('./configuration/feat_descriptor/{}.yaml'.format(args.dataset), 'r') as f:
         para_dict = yaml.load(f, Loader=yaml.SafeLoader)
 
     para_dict = merge_config(para_dict, args)
--- mmad_training.py
+++ mmad_training.py
@@ -1,3 +1,18 @@
 import torch
 import torch.nn as nn
+import yaml
+from configuration.mmad.config import parse_arguments_mmad 
+from tools.utilize import *
+
+if __name__ == '__main__':
+
+    args = parse_arguments_mmad() 
+
+    with open('./configuration/mmad/{}.yaml'.format(args.dataset), 'r') as f:
+        para_dict = yaml.load(f, Loader=yaml.SafeLoader)
+    
+    para_dict = merge_config(para_dict, args)
+    print(para_dict)
+
+    #TODO: Data Loader
 
