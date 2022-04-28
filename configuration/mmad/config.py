cc6cba6ebd4ef438caec1b26a91a09212654647f
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-04-28 09:08:26 +0800

    update

--- configuration/mmad/config.py
+++ configuration/mmad/config.py
@@ -4,7 +4,7 @@ __all__ = ['parse_arguments_mmad']
 
 def parse_arguments_mmad():
     parser = argparse.ArgumentParser() 
-    parser.add_argument('--object-id', default=None, type=int)
+    parser.add_argument('--class-name', default=None, type=str)
     parser.add_argument('--batch-size', default=None, type=int)
     parser.add_argument('--lr', default=None, type=float)
     parser.add_argument('--gpu-id', default=None, type=int)
@@ -12,6 +12,7 @@ def parse_arguments_mmad():
     parser.add_argument('--data-path', default=None, type=str)
     parser.add_argument('--dataset', default=None, type=str)
     parser.add_argument('--fusion-method', default=None, type=str)
+    parser.add_argument('--cl', action='store_true', default=False)
     
     args = parser.parse_args()
     return args
