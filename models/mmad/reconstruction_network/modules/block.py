c1b34d8f87f30b902cae7ad2e42c068e629e7044
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-05-01 16:56:15 +0800

    update

--- models/mmad/reconstruction_network/modules/block.py
+++ models/mmad/reconstruction_network/modules/block.py
@@ -1,2 +1,11 @@
 import torch
 import torch.nn as nn
+
+__all__ = ['EncBlock']
+
+class EncBlock(nn.Module):
+    def __init__(self):
+        super(EncBlock).__init__()
+    
+    def forward(self, x):
+        pass
