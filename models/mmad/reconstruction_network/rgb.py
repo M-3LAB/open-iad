b65740fe1de7e6c6fd4d43c9f7318938b76ddde8
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-05-02 14:00:54 +0800

    update

--- models/mmad/reconstruction_network/rgb.py
+++ models/mmad/reconstruction_network/rgb.py
@@ -6,17 +6,16 @@ __all__ = ['RGBRecons']
 
 class RGBRecons(nn.Module):
 
-    def __init__(self, inc, base_width, expansion_ratio=2):
+    def __init__(self, inc, base_width):
         super(RGBRecons, self).__init__()
         self.inc = inc
         self.base_width = base_width
-        self.exp = expansion_ratio
-        self.stem = StemBlock(inc=self.inc, ouc=self.base_width)
         # Encoder Part
-        self.enc1 = EncBlock(inc=self.base_width, expansion_ratio=self.exp)
-        self.enc2 = EncBlock(inc=self.base_width*2, expansion_ratio=self.exp)
-        self.enc3 = EncBlock(inc=self.base_width*4, expansion_ratio=self.exp)
-        self.enc4 = EncBlock(inc=self.base_width*8, expansion_ratio=self.exp) 
+        self.enc1 = EncBlock(inc=self.inc, ouc=self.base_width)
+        self.enc2 = EncBlock(inc=self.base_width, ouc=self.base_width*2)
+        self.enc3 = EncBlock(inc=self.base_width*2, ouc=self.base_width*4)
+        self.enc4 = EncBlock(inc=self.base_width*4, ouc=base_width*8)
+        self.enc5 = StemBlock(inc=self.base_width*8, ouc=self.base_width*8) 
 
     def forward(self, x):
         pass
