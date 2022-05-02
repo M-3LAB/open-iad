b48b46164c3ba77b456f991aa5135621b407448d
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-05-02 14:59:42 +0800

    update

--- data_io/preprocessing.py
+++ data_io/preprocessing.py
@@ -4,3 +4,11 @@ import os
 import open3d as o3d
 import math
 import argparse
+from PIL import Image
+
+"""
+pc: Point Cloud
+"""
+
+def organized_pc_to_unorganized_pc(organized_pc):
+    return organized_pc.reshape(organized_pc.shape[0] * organized_pc.shape[1], organized_pc.shape[2])
