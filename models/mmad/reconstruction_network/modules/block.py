48a4977870c1963d6066d39a7b8b550766c1016b
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-05-02 09:20:40 +0800

    update

--- models/mmad/reconstruction_network/modules/block.py
+++ models/mmad/reconstruction_network/modules/block.py
@@ -1,11 +1,28 @@
+from tkinter import W
 import torch
 import torch.nn as nn
 
 __all__ = ['EncBlock']
 
+"""
+DRAEM Reconstructive Encoder Block
+"""
 class EncBlock(nn.Module):
-    def __init__(self):
+    def __init__(self, inc, expansion_ratio):
         super(EncBlock).__init__()
+        self.inc = inc
+        self.expansion_ratio = expansion_ratio
+        self.block = nn.Sequential(
+            nn.Conv2d(),
+            nn.BatchNorm2d(),
+            nn.ReLU(),
+            nn.Conv2d(),
+            nn.BatchNorm2d(),
+            nn.ReLU(),
+            nn.MaxPooling()
+        ) 
+        
     
     def forward(self, x):
-        pass
+        output = self.block(x)
+        return output
