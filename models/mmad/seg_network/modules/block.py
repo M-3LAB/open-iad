c216047c919eeeb123e53d53d869cd26204bc9a3
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-05-05 09:59:14 +0800

    update

--- models/mmad/seg_network/modules/block.py
+++ models/mmad/seg_network/modules/block.py
@@ -1,2 +1,15 @@
 import torch
 import torch.nn as nn
+
+__all__ = ['EncSegBlock']
+
+class EncSegBlock(nn.Module):
+
+    def __init__(self, inc, base_width):
+        super().__init__()
+
+        self.inc = inc
+        self.base_width = base_width
+    
+    def forward(self, x):
+        pass
