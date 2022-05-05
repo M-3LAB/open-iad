0ea915f67fa762feaf737b49872b03696575c497
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-05-05 09:20:59 +0800

    udpate

--- loss_function/ssim_loss.py
+++ loss_function/ssim_loss.py
@@ -8,5 +8,14 @@ class SSIMLoss(torch.nn.Module):
         super(SSIMLoss, self).__init__()
         pass
 
+    def create_window(self, windown_size, channel=1):
+        pass
+
+    def gaussian(self):
+        pass
+
+    def calculate_ssim(self):
+        pass
+
     def forward(self, x):
         pass
