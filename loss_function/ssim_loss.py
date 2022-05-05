f0d48ae689d42813327443f77d82ff4e6c619d82
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-05-05 09:07:31 +0800

    update

---
+++ loss_function/ssim_loss.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+
+__all__ = ['SSIMLoss']
+
+class SSIMLoss(torch.nn.Module):
+    def __init__(self):
+        super(SSIMLoss, self).__init__()
+        pass
+
+    def forward(self, x):
+        pass
