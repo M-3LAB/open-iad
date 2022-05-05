9ef7dff3b4006b733e2d9ea6382cfb483f6a68c2
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-05-05 08:19:58 +0800

    update

--- models/mmad/reconstruction_network/depth.py
+++ models/mmad/reconstruction_network/depth.py
@@ -35,5 +35,25 @@ class DepthRecons(nn.Module):
         self.fin_out = nn.Conv2d(base_width, self.fin_ouc, kernel_size=3, padding=1)
 
     def forward(self, x):
+        #Encode
+        d1 = self.enc1(x)
+        d2 = self.enc2(x)
+        d3 = self.enc3(x)
+        d4 = self.enc4(x)
+        d5 = self.enc5(x)
 
-        pass
+        #Decode
+        u1 = self.up1(d5)
+        de1 = self.dec1(u1)
+
+        u2 = self.up2(de1)
+        de2 = self.dec1(u2)
+
+        u3 = self.up3(de2)
+        de3 = self.dec3(u3)
+
+        u4 = self.up4(de3)
+        de4 = self.dec(u4)
+
+        output = self.fin_out(de4)
+        return output
