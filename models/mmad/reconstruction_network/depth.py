70a5f5ee6e42e200212af9873f49703a1ef0a255
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-05-05 08:01:15 +0800

    update

--- models/mmad/reconstruction_network/depth.py
+++ models/mmad/reconstruction_network/depth.py
@@ -12,5 +12,28 @@ class DepthRecons(nn.Module):
         # Final output channel
         self.fin_ouc = fin_ouc
 
+        # Encoder Part
+        self.enc1 = EncBlock(inc=self.inc, ouc=self.base_width)
+        self.enc2 = EncBlock(inc=self.base_width, ouc=self.base_width*2)
+        self.enc3 = EncBlock(inc=self.base_width*2, ouc=self.base_width*4)
+        self.enc4 = EncBlock(inc=self.base_width*4, ouc=base_width*8)
+        self.enc5 = StemBlock(inc=self.base_width*8, ouc=self.base_width*8) 
+
+        #Decoder Part
+        self.up1 = DecUpBlock(inc=self.base_width*8, ouc=self.base_width*8) 
+        self.dec1 = DecDownBlock(inc=self.base_width*8, ouc=self.base_width*4)
+
+        self.up2 = DecUpBlock(inc=self.base_width*4, ouc=self.base_width*4)
+        self.dec2 = DecDownBlock(inc=self.base_with*4, ouc=self.base_width*2)
+
+        self.up3 = DecUpBlock(inc=self.base_width*2, ouc=self.base_with*2)
+        self.dec3 = DecDownBlock(inc=self.base_with*2, ouc=self.base_width)
+
+        self.up4 = DecUpBlock(inc=self.base_width, ouc=self.base_width)
+        self.dec4 = DecDownBlock(inc=self.base_width, ouc=self.base_width)
+
+        self.fin_out = nn.Conv2d(base_width, self.fin_ouc, kernel_size=3, padding=1)
+
     def forward(self, x):
+
         pass
