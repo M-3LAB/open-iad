5e45b746c36cdbf5859f3f9a734cfbbb27c71556
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-05-18 07:45:10 +0800

    update

--- models/mmad/seg_network/modules/block.py
+++ models/mmad/seg_network/modules/block.py
@@ -5,14 +5,27 @@ __all__ = ['EncSegBlock', 'DecSegBlock']
 
 class EncSegBlock(nn.Module):
 
-    def __init__(self, inc, ouc):
+    def __init__(self, inc, ouc, ks=3, padding=1):
         super(EncSegBlock).__init__()
 
         self.inc = inc
         self.ouc = ouc 
+        self.ks = ks
+        self.pad = padding
+
+        self.block = nn.Sequential(
+            nn.Conv2d(),
+            nn.BatchNorm2d(),
+            nn.ReLU(),
+            nn.Conv2d(),
+            nn.BatchNorm2d(),
+            nn.ReLU()
+        )
     
     def forward(self, x):
-        pass
+        output = self.block(x)
+        return output
+
 
 class DecSegBlock(nn.Module):
 
