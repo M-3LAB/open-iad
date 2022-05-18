e8b3808f050960b4c630bf9d699d351b91c01f5d
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-05-18 14:09:49 +0800

    update

--- loss_function/ssim_loss.py
+++ loss_function/ssim_loss.py
@@ -5,18 +5,19 @@ from math import exp
 __all__ = ['SSIMLoss']
 
 class SSIMLoss(nn.Module):
-    def __init__(self, window_size, size_average, sigma):
+    def __init__(self, window_size, size_average, sigma, channel_number=1):
         super(SSIMLoss, self).__init__()
 
         self.window_size = window_size
         self.size_average = size_average
         self.sigma = sigma
+        self.channel_number = channel_number
         
 
-    def create_window(self, channel=1):
+    def create_window(self):
         window_1d = self.gaussian(self.window_size, 1.5).unsqueeze(1)
         window_2d = window_1d.mm(window_1d.t()).float().unsqueeze(0).unsqueeze(0)
-        window = window_2d.expand(channel, 1, self.window_size, self.window_size).contiguous()
+        window = window_2d.expand(self.channel_number, 1, self.window_size, self.window_size).contiguous()
         return window
 
     def gaussian(self):
