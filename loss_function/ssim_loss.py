002d18c1e1bc48e6d663838cb22deb8522caded5
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-05-18 14:00:04 +0800

    add ssim loss

--- loss_function/ssim_loss.py
+++ loss_function/ssim_loss.py
@@ -1,18 +1,24 @@
 import torch
 import torch.nn as nn
+from math import exp
 
 __all__ = ['SSIMLoss']
 
 class SSIMLoss(nn.Module):
-    def __init__(self):
+    def __init__(self, window_size, size_average, sigma):
         super(SSIMLoss, self).__init__()
-        pass
 
-    def create_window(self, windown_size, channel=1):
+        self.window_size = window_size
+        self.size_average = size_average
+        self.sigma = sigma
+        
+
+    def create_window(self, channel=1):
         pass
 
     def gaussian(self):
-        pass
+        gauss = torch.Tensor([exp(-(x - self.window_size//2)**2/float(2* self.sigma**2)) for x in range(self.window_size)])
+        return gauss/gauss.sum()
 
     def calculate_ssim(self):
         pass
