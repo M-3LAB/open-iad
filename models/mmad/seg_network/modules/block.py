3dbb46b0979d5d4822f012cfdfa8e0aa19c6b887
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-05-18 08:54:37 +0800

    update

--- models/mmad/seg_network/modules/block.py
+++ models/mmad/seg_network/modules/block.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 
-__all__ = ['EncSegBlock', 'DecSegBlock']
+__all__ = ['EncSegBlock', 'DecUpSegBlock', 'DecDownSegBlock']
 
 class EncSegBlock(nn.Module):
 
@@ -27,13 +27,36 @@ class EncSegBlock(nn.Module):
         return output
 
 
-class DecSegBlock(nn.Module):
+class DecDownSegBlock(nn.Module):
 
-    def __init__(self, inc, ouc):
-        super(DecSegBlock).__init__()
+    def __init__(self, inc, ouc, ks=3, padding=1):
+        super(DecDownSegBlock).__init__()
 
         self.inc = inc
         self.ouc = ouc
+        self.ks = ks
+        self.pad = padding    
+
+        self.block = nn.Sequential(
+            nn.Conv2d(self.inc, self.ouc, kernel_size=self.ks, padding=self.pad),
+            nn.BatchNorm2d(self.ouc),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(self.ouc, self.ouc, kernel_size=self.ks, padding=self.pad),
+            nn.BatchNorm2d(self.ouc),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        pass
+
+class DecUpSegBlock(nn.Module):
+
+    def __init__(self, inc, ouc, scale_factor=2, mode='bilinear', align_corners=True):
+        super(DecUpSegBlock).__init__()
+
+        self.inc = inc
+        self.ouc = ouc
+        self.scale_factor = scale_factor
     
     def forward(self, x):
         pass
