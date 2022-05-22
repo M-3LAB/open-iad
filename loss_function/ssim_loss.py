2c423abcc10220151444c936a4588557336ade7b
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-05-22 11:33:30 +0800

    update

--- loss_function/ssim_loss.py
+++ loss_function/ssim_loss.py
@@ -7,7 +7,8 @@ __all__ = ['SSIMLoss']
 
 class SSIMLoss(nn.Module):
     def __init__(self, window_size, size_average, sigma, 
-                 channel_number=1, device=None, value_range=None):
+                 channel_number=1, device=None):
+
         super(SSIMLoss, self).__init__()
 
         self.window_size = window_size
@@ -15,7 +16,6 @@ class SSIMLoss(nn.Module):
         self.sigma = sigma
         self.channel_number = channel_number
         self.window = self.create_window().to(device)
-        self.value_range = self.define_val_range(predefined_value_range=value_range)
         
     def create_window(self):
         window_1d = self.gaussian(self.window_size, 1.5).unsqueeze(1)
@@ -27,13 +27,26 @@ class SSIMLoss(nn.Module):
         gauss = torch.Tensor([exp(-(x - self.window_size//2)**2/float(2* self.sigma**2)) for x in range(self.window_size)])
         return gauss/gauss.sum()
 
-    def define_val_range(self, predefined_value_range=None):
+    def define_val_range(self, img, predefined_value_range=None):
         if predefined_value_range is None: 
-            pass
-        else:
-            return predefined_value_range
+            if torch.max(img) > 128:
+                max_value = 255
+            else: 
+                max_value = 1
+            
+            if torch.min(img) < -0.5:
+                min_value = -1
+            else:
+                min_value = 0
 
-    def calculate_ssim(self, img_a, img_b, window):
+            value_range = max_value - min_value
+
+        else:
+            value_range = predefined_value_range
+
+        return value_range
+
+    def calculate_ssim(self, img_a, img_b, window, config_value_range=None):
         padding = self.window_size // 2
 
         mu1 = F.conv2d(img_a, window, padding=padding, groups=self.channel)
@@ -47,8 +60,10 @@ class SSIMLoss(nn.Module):
         sigma2_sq = F.conv2d(img_a * img_b, window, padding=padding, groups=self.channel) - mu2_sq
         sigma12 = F.conv2d(img_a * img_b, window, padding=padding, groups=self.channel) - mu1_mu2
 
-        c1 = (0.01 * self.value_range) ** 2
-        c2 = (0.03 * self.value_range) ** 2
+        value_range = self.define_val_range(img=img_a, predefined_value_range=config_value_range)
+
+        c1 = (0.01 * value_range) ** 2
+        c2 = (0.03 * value_range) ** 2
 
         v1 = 2.0 * sigma12 + c2
         v2 = sigma1_sq + sigma2_sq + c2
