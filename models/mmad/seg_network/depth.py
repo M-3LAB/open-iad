a2b06d2497c97dcfac21f882e770c1ff24797c74
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-05-22 15:07:34 +0800

    update

--- models/mmad/seg_network/depth.py
+++ models/mmad/seg_network/depth.py
@@ -1,12 +1,15 @@
 import torch
 import torch.nn as nn
+from seg_network.modules.block import * 
 
 __all__ = ['DepthSeg']
 
 class DepthSeg(nn.Module):
-    def __init__(self):
+    def __init__(self, inc, base_width):
         super(DepthSeg).__init__()
-        pass
+
+        self.inc = inc
+        self.base_width = base_width
 
     def forward(self, x):
         pass
--- models/mmad/seg_network/rgb.py
+++ models/mmad/seg_network/rgb.py
@@ -1,12 +1,16 @@
 import torch
 import torch.nn as nn
+from seg_network.modules.block import * 
 
 __all__=['RGBSeg']
 
 class RGBSeg(nn.Module):
-    def __init__(self):
+    def __init__(self, inc, base_width):
         super(RGBSeg, self).__init__()
-        pass
+        self.inc = inc
+        self.base_width = base_width
+        # Encoder Part
+        
 
     def forward(self, x):
         pass
