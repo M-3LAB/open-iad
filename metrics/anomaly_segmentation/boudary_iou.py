fce909c43354be4f90f8e50455910b0b24c7c540
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-05-28 10:18:01 +0800

    add boundary iou

---
+++ metrics/anomaly_segmentation/boudary_iou.py
@@ -0,0 +1,40 @@
+import cv2
+import numpy as np
+
+__all__ = ['mask_to_boundary', 'boundary_iou']
+
+def mask_to_boundary(mask, dilation_ratio=0.02):
+    """
+    Convert binary mask to boundary mask.
+    :param mask (numpy array, uint8): binary mask
+    :param dilation_ratio (float): ratio to calculate dilation = dilation_ratio * image_diagonal
+    :return: boundary mask (numpy array)
+    """
+    h, w = mask.shape
+    img_diag = np.sqrt(h ** 2 + w ** 2)
+    dilation = int(round(dilation_ratio * img_diag))
+    if dilation < 1:
+        dilation = 1
+    # Pad image so mask truncated by the image border is also considered as boundary.
+    new_mask = cv2.copyMakeBorder(mask, 1, 1, 1, 1, cv2.BORDER_CONSTANT, value=0)
+    kernel = np.ones((3, 3), dtype=np.uint8)
+    new_mask_erode = cv2.erode(new_mask, kernel, iterations=dilation)
+    mask_erode = new_mask_erode[1 : h + 1, 1 : w + 1]
+    # G_d intersects G in the paper.
+    return mask - mask_erode
+
+
+def boundary_iou(gt, dt, dilation_ratio=0.02):
+    """
+    Compute boundary iou between two binary masks.
+    :param gt (numpy array, uint8): binary mask
+    :param dt (numpy array, uint8): binary mask
+    :param dilation_ratio (float): ratio to calculate dilation = dilation_ratio * image_diagonal
+    :return: boundary iou (float)
+    """
+    gt_boundary = mask_to_boundary(gt, dilation_ratio)
+    dt_boundary = mask_to_boundary(dt, dilation_ratio)
+    intersection = ((gt_boundary * dt_boundary) > 0).sum()
+    union = ((gt_boundary + dt_boundary) > 0).sum()
+    boundary_iou = intersection / union
+    return boundary_iou
