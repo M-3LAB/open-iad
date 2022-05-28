b2fa444a8d73ea40b2d9a6d14a15e1fb9f1d6e74
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-05-28 12:57:34 +0800

    update

--- mmad_test.py
+++ mmad_test.py
@@ -1,6 +1,12 @@
 import torch
 import torch.nn as nn
 import os
+from torch.utils.data import DataLoader
+from models.mmad.reconstruction_network.depth import DepthRecons
+from models.mmad.reconstruction_network.rgb import RGBRecons 
+from models.mmad.seg_network.depth import DepthSeg
+from models.mmad.seg_network.rgb import RGBSeg
+
 
 if __name__ == '__main__':
     pass
