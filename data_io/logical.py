bcc2e0a1b6e9bc50c2f66dbdddea927f040d0111
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-06-01 10:02:41 +0800

    update

--- data_io/logical.py
+++ data_io/logical.py
@@ -2,3 +2,10 @@ import torch
 import torch.nn as nn
 
 __all__ = ['Logical']
+
+class Logical(nn.Module):
+    def __init__(self):
+        super().__init__()
+    
+    def forward(self):
+        pass
