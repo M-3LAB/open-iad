6cf32a8dd60c57f47230c1b5414362eaaab651e4
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-06-15 10:29:07 +0800

    add legacy_code

---
+++ legacy_code/mmad_test.py
@@ -0,0 +1,256 @@
+import torch
+import torch.nn as nn
+import os
+from torch.utils.data import DataLoader
+from models.mmad.reconstruction_network.depth import DepthRecons
+from models.mmad.reconstruction_network.rgb import RGBRecons 
+from models.mmad.seg_network.depth import DepthSeg
+from models.mmad.seg_network.rgb import RGBSeg
+from configuration.mmad.config import parse_arguments_mmad 
+from tools.utilize import * 
+from data_io.mvtec3d import MVTec3D, MVTecCL3D, mvtec3d_classes
+import yaml
+import numpy as np
+from metrics.auc_precision_recall import get_auroc, get_precision_recall, get_ap
+from metrics.mvtec3d.au_pro import calculate_au_pro
+
+if __name__ == '__main__':
+    args = parse_arguments_mmad() 
+
+    args = parse_arguments_mmad() 
+
+    with open('./configuration/mmad/{}.yaml'.format(args.dataset), 'r') as f:
+        para_dict = yaml.load(f, Loader=yaml.SafeLoader)
+    
+    para_dict = merge_config(para_dict, args)
+    print(para_dict)
+
+    seed_everything(para_dict['seed'])
+
+    device, device_ids = parse_device_list(para_dict['gpu_ids'], int(para_dict['gpu_id']))
+    device = torch.device("cuda", device)
+
+    mvtec3d_transform = {'data_size':para_dict['data_size'],
+                         'mask_size':para_dict['mask_size']}
+    
+    if para_dict['class_names'] == 'all':
+        class_names = mvtec3d_classes() 
+    else: 
+        class_names = para_dict['class_names']
+
+    rgb_ck_path = os.path.join(para_dict['ck_path'], 'rgb') 
+    depth_ck_path = os.path.join(para_dict['ck_path'], 'depth')
+
+    rgb_recons_ck_path = os.path.join(rgb_ck_path, 'recons')
+    rgb_seg_ck_path = os.path.join(rgb_ck_path, 'seg')
+
+    depth_recons_ck_path = os.path.join(depth_ck_path, 'recons')
+    depth_seg_ck_path = os.path.join(depth_ck_path, 'seg')
+
+    rgb_obj_ap_pixel_list = []
+    rgb_obj_auroc_pixel_list = []
+    rgb_obj_ap_image_list = []
+    rgb_obj_auroc_image_list = []
+
+    depth_obj_ap_pixel_list = []
+    depth_obj_auroc_pixel_list = []
+    depth_obj_ap_image_list = []
+    depth_obj_auroc_image_list = []
+
+    img_dim = mvtec3d_transform['data_size']
+
+    for cls in class_names:
+        rgb_recons = RGBRecons(inc=3, fin_ouc=3).to(device)
+        rgb_seg = RGBSeg(inc=3, fin_ouc=2).to(device)
+
+        depth_recons = DepthRecons(inc=3, fin_ouc=3).to(device)
+        depth_seg = DepthSeg(inc=3, fin_ouc=3).to(device)
+
+        load_model(model=rgb_recons, file_path=rgb_recons_ck_path, description=cls+str(para_dict['num_epochs'])) 
+        load_model(model=rgb_seg, file_path=rgb_seg_ck_path, description=cls+str(para_dict['num_epochs'])) 
+
+        load_model(model=depth_recons, file_path=depth_recons_ck_path, description=cls+str(para_dict['num_epochs'])) 
+        load_model(model=depth_seg, file_path=depth_seg_ck_path, description=cls+str(para_dict['num_epochs'])) 
+
+        rgb_recons.eval()
+        rgb_seg.eval()
+
+        depth_recons.eval()
+        depth_seg.eval() 
+
+        if para_dict['dataset'] == 'mvtec3d':
+            if para_dict['cl']:
+                valid_dataset = MVTecCL3D()
+            else: 
+                valid_dataset = MVTec3D(data_path=para_dict['data_path'], class_names=cls,
+                                        phase='test', depth_duplicate=para_dict['depth_duplicate'],
+                                        data_transform=mvtec3d_transform) 
+
+        valid_loader = DataLoader(valid_dataset, num_workers=para_dict['num_workers'],
+                                  batch_size=para_dict['batch_size'], shuffle=False) 
+
+        total_rgb_pixel_scores = np.zeros(img_dim * img_dim * len(valid_dataset))
+        total_rgb_gt_pixel_scores = np.zeros(img_dim * img_dim * len(valid_dataset))
+
+        total_depth_pixel_scores = np.zeros(img_dim * img_dim * len(valid_dataset))
+        total_depth_gt_pixel_scores = np.zeros(img_dim * img_dim * len(valid_dataset))
+        
+        mask_cnt = 0
+
+        rgb_anomaly_score_gt = []
+        rgb_anomaly_score_prediction = []
+
+        depth_anomaly_score_gt = []
+        depth_anomaly_score_prediction = []
+        
+        
+        for idx, batch in enumerate(valid_loader):
+            rgb = batch['rgb'].to(device)
+            depth = batch['depth'].to(device)
+            mask = batch['mask'].to(device)
+            label = batch['label'].to(device)
+
+            rgb_anomaly_score_gt.append(label)
+            depth_anomaly_score_gt.append(label)
+
+            # mask: convert torch format into open-cv format 
+            mask_cv = mask.detach().numpy()[0. :, :, :].transpose((1, 2, 0))
+
+            if para_dict['backbone_model'] == 'DRAEM':
+
+                rgb_hat = RGBRecons(rgb)
+                rgb_joined = torch.cat((rgb, rgb_hat), dim=1)
+                rgb_out_mask = RGBSeg(rgb_joined)
+                rgb_out_mask_softmax = torch.softmax(rgb_out_mask, dim=1)
+                
+                depth_hat = DepthRecons(depth) 
+                depth_joined = torch.cat((depth, depth_hat), dim=1)
+                depth_out_mask = DepthSeg(depth_joined)
+                depth_out_mask_softmax = torch.softmax(depth_out_mask, dim=1)
+
+                # segmentation output mask size  
+                rgb_out_mask_cv = rgb_out_mask_softmax[0, 1, :, :].detach().cpu().numpy()
+                depth_out_mask_cv = depth_out_mask_softmax[0, 1, :, :].detach().cpu().numpy()
+                
+                # smoothing mask and convert it into numpy format 
+                rgb_out_mask_averaged = torch.nn.functional_avg_pool2d(input=rgb_out_mask_softmax,
+                                                                       kernel_size=para_dict['smooth_kernel_size'],
+                                                                       stride=1,
+                                                                       padding=para_dict['smooth_kernel_size']//2).cpu().detach().numpy
+
+                depth_out_mask_averaged = torch.nn.functional_avg_pool2d(input=depth_out_mask_softmax,
+                                                                         kernel_size=para_dict['smooth_kernel_size'],
+                                                                         stride=1,
+                                                                         padding=para_dict['smooth_kernel_size']//2).cpu().detach().numpy
+                
+                rgb_score = np.max(rgb_out_mask_averaged)
+                depth_score = np.max(depth_out_mask_averaged)
+
+                rgb_anomaly_score_prediction.append(rgb_score)
+                depth_anomaly_score_prediction.append(depth_score)
+
+                flat_rgb_true_mask = mask_cv.flatten()                                            
+                flat_rgb_out_mask = rgb_out_mask_cv.flatten()
+
+                flat_depth_true_mask = mask_cv.flatten() 
+                flat_depth_out_mask = depth_out_mask_cv.flatten()  
+
+                total_rgb_pixel_scores[mask_cnt * img_dim * img_dim: (mask_cnt + 1) * img_dim * img_dim] = flat_rgb_out_mask
+                total_rgb_gt_pixel_scores[mask_cnt * img_dim * img_dim: (mask_cnt + 1) * img_dim * img_dim] = flat_rgb_true_mask
+
+                total_depth_pixel_scores[mask_cnt * img_dim * img_dim: (mask_cnt + 1) * img_dim * img_dim] = flat_depth_out_mask
+                total_depth_gt_pixel_scores[mask_cnt * img_dim * img_dim: (mask_cnt + 1) * img_dim * img_dim] = flat_depth_true_mask
+
+                mask_cnt += 1
+
+        
+        rgb_anomaly_score_prediction = np.array(rgb_anomaly_score_prediction)
+        rgb_anomaly_score_gt = np.array(rgb_anomaly_score_gt)
+
+        depth_anomaly_score_prediction = np.array(depth_anomaly_score_prediction)
+        depth_anomaly_score_gt = np.array(depth_anomaly_score_gt)
+
+        rgb_auroc = get_auroc(rgb_anomaly_score_gt, rgb_anomaly_score_prediction)
+        depth_auroc = get_auroc(depth_anomaly_score_gt, depth_anomaly_score_prediction)
+
+        rgb_ap = get_ap(rgb_anomaly_score_gt, rgb_anomaly_score_prediction)
+        depth_ap = get_ap(depth_anomaly_score_gt, depth_anomaly_score_prediction)
+
+        total_rgb_gt_pixel_scores = total_rgb_gt_pixel_scores.astype(np.uint8)
+        total_rgb_gt_pixel_scores = total_rgb_pixel_scores[: img_dim * img_dim * mask_cnt]
+        total_rgb_pixel_scores = total_rgb_pixel_scores[:img_dim * img_dim * mask_cnt]
+
+        total_depth_gt_pixel_scores = total_depth_gt_pixel_scores.astype(np.uint8)
+        total_depth_gt_pixel_scores = total_depth_pixel_scores[: img_dim * img_dim * mask_cnt]
+        total_depth_pixel_scores = total_depth_pixel_scores[: img_dim * img_dim * mask_cnt]
+
+        rgb_auroc_pixel = get_auroc(total_rgb_gt_pixel_scores, total_rgb_pixel_scores)
+        depth_auroc_pixel = get_auroc(total_depth_gt_pixel_scores, total_depth_pixel_scores)
+
+        rgb_ap_pixel = get_ap(total_rgb_gt_pixel_scores, total_rgb_pixel_scores)
+        depth_ap_pixel = get_ap(total_depth_gt_pixel_scores, total_depth_pixel_scores)
+
+        rgb_obj_ap_pixel_list.append(rgb_ap_pixel)
+        rgb_obj_auroc_pixel_list.append(rgb_auroc_pixel)
+        rgb_obj_ap_image_list.append(rgb_ap)
+        rgb_obj_auroc_image_list.append(rgb_auroc)
+
+        depth_obj_ap_pixel_list.append(depth_ap_pixel)
+        depth_obj_auroc_pixel_list.append(depth_auroc_pixel)
+        depth_obj_ap_image_list.append(depth_ap)
+        depth_obj_auroc_image_list.append(depth_auroc)
+
+        print("==============================")
+        print(f'{cls} begin evaluation')
+        print("==============================")
+
+        print(f'RGB AP: {rgb_ap}')
+        print(f'RGB AUROC: {rgb_auroc}')
+        print(f'RGB Pixel AP: {rgb_ap_pixel}')
+        print(f'RGB Pixel AUROC: {rgb_auroc_pixel}')
+
+        print("==============================")
+
+        print(f'Depth AP: {depth_ap}')
+        print(f'Depth AUROC: {depth_auroc}')
+        print(f'Depth Pixel AP: {depth_ap_pixel}')
+        print(f'Depth Pixel AUROC: {depth_auroc_pixel}')
+
+        print("==============================")
+    
+    print("==============================")
+    print('Overall Evaluation Started')
+    print("==============================")
+
+    rgb_mean_ap = np.mean(rgb_obj_ap_image_list)
+    rgb_mean_auroc = np.mean(rgb_obj_auroc_image_list)
+
+    rgb_mean_ap_pixel = np.mean(rgb_obj_ap_pixel_list)    
+    rgb_mean_auroc_pixel = np.mean(rgb_obj_auroc_pixel_list)
+
+    depth_mean_ap = np.mean(depth_obj_ap_image_list)
+    depth_mean_auroc = np.mean(depth_obj_auroc_image_list)
+
+    depth_mean_ap_pixel = np.mean(depth_obj_ap_pixel_list)    
+    depth_mean_auroc_pixel = np.mean(depth_obj_auroc_pixel_list)
+
+    print(f'RGB Mean AP: {rgb_mean_ap}')
+    print(f'RGB Mean AUROC: {rgb_mean_auroc}')
+    print(f'RGB Mean Pixel AP: {rgb_mean_ap_pixel}')
+    print(f'RGB Mean Pixel AUROC: {rgb_mean_auroc_pixel}')
+
+    print(f'Depth Mean AP: {depth_mean_ap}')
+    print(f'Depth Mean AUROC: {depth_mean_auroc}')
+    print(f'Depth Mean Pixel AP: {depth_mean_ap_pixel}')
+    print(f'Depth Mean Pixel AUROC: {depth_mean_auroc_pixel}')
+    
+
+
+
+        
+
+
+                
+
+                 
+            
---
+++ legacy_code/mmad_training.py
@@ -0,0 +1,180 @@
+import torch
+import torch.nn as nn
+import yaml
+from configuration.mmad.config import parse_arguments_mmad 
+from tools.utilize import *
+from data_io.mvtec3d import MVTec3D, MVTecCL3D, mvtec3d_classes
+from torch.utils.data import DataLoader
+#from torch.utils.data.sampler import SubsetRandomSampler
+import os
+from models.mmad.reconstruction_network.depth import DepthRecons
+from models.mmad.reconstruction_network.rgb import RGBRecons 
+from models.mmad.seg_network.depth import DepthSeg
+from models.mmad.seg_network.rgb import RGBSeg
+from tools.utilize import * 
+from loss_function.ssim_loss import SSIMLoss 
+from loss_function.focal_loss import FocalLoss
+
+if __name__ == '__main__':
+
+    args = parse_arguments_mmad() 
+
+    with open('./configuration/mmad/{}.yaml'.format(args.dataset), 'r') as f:
+        para_dict = yaml.load(f, Loader=yaml.SafeLoader)
+    
+    para_dict = merge_config(para_dict, args)
+    print(para_dict)
+
+    seed_everything(para_dict['seed'])
+
+    device, device_ids = parse_device_list(para_dict['gpu_ids'], int(para_dict['gpu_id']))
+    device = torch.device("cuda", device)
+
+    mvtec3d_transform = {'data_size':para_dict['data_size'],
+                         'mask_size':para_dict['mask_size']}
+    
+    if para_dict['class_names'] == 'all':
+        class_names = mvtec3d_classes() 
+    else: 
+        class_names = para_dict['class_names']
+
+
+    rgb_ck_path = os.path.join(para_dict['ck_path'], 'rgb') 
+    depth_ck_path = os.path.join(para_dict['ck_path'], 'depth')
+
+    rgb_recons_ck_path = os.path.join(rgb_ck_path, 'recons')
+    create_folders(rgb_recons_ck_path)
+
+    rgb_seg_ck_path = os.path.join(rgb_ck_path, 'seg')
+    create_folders(rgb_seg_ck_path)
+
+    depth_recons_ck_path = os.path.join(depth_ck_path, 'recons')
+    create_folders(depth_recons_ck_path)
+
+    depth_seg_ck_path = os.path.join(depth_ck_path, 'seg')
+    create_folders(depth_seg_ck_path)
+
+    l2_loss = torch.nn.MSELoss().to(device)
+    focal_loss = FocalLoss().to(device)
+    ssim_loss = SSIMLoss().to(device)
+    
+
+    for cls in class_names: 
+        rgb_recons = RGBRecons(inc=3, fin_ouc=3).to(device)
+        depth_recons = DepthRecons(inc=3, fin_ouc=2).to(device)
+
+        rgb_recons.apply(draem_weights_init)
+        depth_recons.apply(draem_weights_init)
+        
+        rgb_seg = RGBSeg(inc=6, fin_ouc=2).to(device)
+        depth_seg = DepthSeg(inc=6, fin_ouc=2).to(device)
+
+        rgb_seg.apply(draem_weights_init)
+        depth_seg.apply(draem_weights_init)
+
+        rgb_optimizer = torch.optim.Adam([{"params": rgb_seg.parameters(), "lr": para_dict['lr']},
+                                          {"params": rgb_recons.parameters(), "lr": para_dict['lr']}])
+
+        depth_optimizer = torch.optim.Adam([{"params": depth_seg.parameters(), "lr": para_dict['lr']},
+                                            {"params": depth_recons.parameters(), "lr": para_dict['lr']}])
+        
+        rgb_scheduler = torch.optim.lr_scheduler.MultiStepLR(rgb_optimizer, 
+                                                             milestones=[para_dict['milestones_lower'], para_dict['milestones_higher']],
+                                                             gamma=para_dict['gamma'])
+
+        depth_scheduler = torch.optim.lr_scheduler.MultiStepLR(depth_optimizer, 
+                                                               milestones=[para_dict['milestones_lower'], para_dict['milestones_higher']],
+                                                               gamma=para_dict['gamma'])
+        
+        if para_dict['dataset'] == 'mvtec3d':
+            if para_dict['cl']:
+                train_dataset = MVTecCL3D()
+                valid_dataset = MVTecCL3D()
+            else:
+                train_dataset = MVTec3D(data_path=para_dict['data_path'], class_names=cls,
+                                        phase='train', 
+                                        depth_duplicate=para_dict['depth_duplicate'], 
+                                        data_transform=mvtec3d_transform)
+
+                #valid_dataset = MVTec3D(data_path=para_dict['data_path'], class_names=cls,
+                #                        phase='test', depth_duplicate=para_dict['depth_duplicate'],
+                #                        data_transform=mvtec3d_transform) 
+
+        if not para_dict['cl']:
+            train_loader = DataLoader(train_dataset,
+                                      batch_size=para_dict['batch_size'],
+                                      num_workers=para_dict['num_workers'],
+                                      shuffle=True)
+
+            valid_loader = DataLoader(valid_dataset, num_workers=para_dict['num_workers'],
+                                      batch_size=para_dict['batch_size'], shuffle=False) 
+        
+        for epoch in range(para_dict['num_epochs']):
+            for idx, batch in enumerate(train_loader): 
+                if para_dict['aug_method'] == 'DRAEM':
+                    rgb = batch['rgb'].to(device)
+                    aug_rgb = batch['aug_rgb'].to(device)
+                    depth = batch['depth'].to(device)
+                    aug_depth = batch['aug_depth'].to(device)
+                    aug_mask = batch['aug_mask'].to(device)
+
+                    rgb_hat = rgb_recons(rgb)
+                    rgb_joined = torch.cat((rgb_hat, aug_rgb), dim=1)
+
+                    depth_hat = depth_recons(depth)
+                    depth_joined = torch.cat((depth_hat, aug_depth), dim=1)
+
+                    rgb_output_mask = rgb_seg(rgb_joined) 
+                    rgb_output_mask_logit = torch.softmax(rgb_output_mask, dim=1)
+
+                    depth_output_mask = depth_seg(depth_joined)
+                    depth_output_mask_logit = torch.softmax(depth_output_mask, dim=1)
+
+                    rgb_recons_loss = l2_loss(rgb, rgb_hat)                   
+                    depth_recons_loss = l2_loss(depth, depth_hat)
+
+                    rgb_ssim_loss = ssim_loss(rgb, rgb_hat) 
+                    depth_ssim_loss = ssim_loss(depth, depth_hat)
+
+                    rgb_seg_loss = focal_loss(rgb_output_mask_logit, aug_mask) 
+                    depth_seg_loss = focal_loss(depth_output_mask_logit, aug_mask)
+
+                    rgb_total_loss = (para_dict['lambda_recon'] * rgb_recons_loss + 
+                                      para_dict['lambda_seg'] * rgb_seg_loss + 
+                                      para_dict['lambda_ssim'] * rgb_ssim_loss)
+
+                    depth_total_loss = (para_dict['lambda_recon'] * depth_recons_loss + 
+                                        para_dict['lambda_seg'] * depth_seg_loss + 
+                                        para_dict['lambda_ssim'] * depth_ssim_loss)
+                    
+                    total_loss = (para_dict['lambda_rgb'] * rgb_total_loss + 
+                                  para_dict['lambda_depth'] * depth_total_loss)
+
+                    rgb_optimizer.zero_grad()
+                    depth_optimizer.zero_grad()
+
+                    total_loss.backward()
+                    
+                    rgb_optimizer.step()
+                    depth_optimizer.step()
+
+                    infor = '\r{}[Epoch {} / {}] [Batch {}/{}] '.format(
+                        '', epoch+1, para_dict['number_epochs'], idx+1, len(train_loader) / para_dict['batch_size'])
+                    
+                    infor = '{} [RGB Recon Loss: {:.4f}] [RGB Seg Loss: {:.4f}] [RGB SSIM Loss: {:.4f}]'.format(
+                        infor, rgb_recons_loss.item(), rgb_ssim_loss.item(), rgb_seg_loss.item()
+                    ) 
+
+                    infor = '{} [Depth Recon Loss: {:.4f}] [Depth Seg Loss: {:.4f}] [Depth SSIM Loss: {:.4f}]'.format(
+                        infor, depth_recons_loss.item(), depth_ssim_loss.item(), depth_seg_loss.item()
+                    ) 
+
+            rgb_scheduler.step()
+            depth_scheduler.step()
+
+            #TODO: save rgb and depth model
+            save_model(model=rgb_recons, file_path=rgb_recons_ck_path, infor=cls+str(epoch))
+            save_model(model=rgb_seg, file_path=rgb_seg_ck_path, infor=cls+str(epoch))
+            save_model(model=depth_recons, file_path=depth_recons_ck_path, infor=cls+str(epoch))
+            save_model(model=depth_seg, file_path=depth_seg_ck_path, infor=cls+str(epoch))
+            
