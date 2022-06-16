8904f306de14345889c3ec28c87889c5c44c2302
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-06-16 16:40:13 +0800

    update

--- models/common/feat.py
+++ models/common/feat.py
@@ -1,2 +1,15 @@
 import torch
 import torch.nn as nn
+
+__all__ = ['FeatureExtractor']
+
+class FeatureExtractor(nn.Module):
+    def __init__(self, backbone, layers):
+        super().__init__()
+        self.backbone = backbone
+        self.layers = layers
+        # collect the output dimension from each layer
+        self.output_dims = []
+        self._features = {layer: torch.empty(0) for layer in self.layers}
+
+        
