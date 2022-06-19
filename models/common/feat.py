f79554085f34f99c9a1c9e71c6b8a2e61cb0fb16
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-06-19 16:00:59 +0800

    update

--- models/common/feat.py
+++ models/common/feat.py
@@ -12,6 +12,17 @@ class FeatureExtractor(nn.Module):
         self.output_dims = []
         self._features = {layer: torch.empty(0) for layer in self.layers}
 
+        for layer_id in self.layers:
+            layer = dict(*self.backbone.named_modules)[layer_id]
+            layer.register_forward_hook(self.get_layer_features(layer_id))
+            # Get the output dimension 
+            layer_modules = [*layer.modules()]
+            for idx in reversed(range(len(layer_modules))):
+                if hasattr(layer_modules[idx], "out_channels"):
+                    self.output_dims.append(layer_modules[idx].out_channels)
+                    break 
+            
+
     def get_layer_features(self, layer_id):
         """
         Get Layer Features
