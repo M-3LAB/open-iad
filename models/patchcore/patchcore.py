df1783c702e0065836d9f03ae8bb2ab67cf0c4d4
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-06-20 08:11:47 +0800

    update

--- models/patchcore/patchcore.py
+++ models/patchcore/patchcore.py
@@ -1,14 +1,13 @@
 import torch
 import torch.nn as nn
 import numpy as np
-from models.resnet_extractor import ResNetExtractor 
+from models.common.feat import FeatureExtractor
 
 __all__ = ['PatchCore']
 
 class PatchCore(nn.Module):
 
-    def __init__(self, backbone_name, device, layer_hook, layer_indices,
-                 channel_indices):
+    def __init__(self, input_size, backbone_name, device, layers, num_neighbours):
 
         super(PatchCore).__init__()
 
@@ -16,27 +15,14 @@ class PatchCore(nn.Module):
         Args:
             backbone: The name of the desired backbone, i.e., ['resnet18', 'wide_resnet'] 
             device: GPU
-            channel indices: A tensor with the desired channel indices to extract from the backbone
-            layer_hook: A function layer_hook: A function that runs on each layer of the resnet before
-                concatenating them. 
-            layer_indices: A list of indices with the desired layers to include in the
-                embedding vectors.
         """
 
         self.device = device
         self.backbone_name = backbone_name
-        self.feat_extractor = ResNetExtractor(device=self.device, 
-                                              backbone_name=self.backbone_name).to(self.device) 
-
-        self.layer_hook = torch.nn.AvgPool2d(3, 1, 1) if layer_hook is None else layer_hook
-        self.layer_indices = [1, 2] if layer_indices is None else layer_indices 
-
-        self.channel_indices = channel_indices.to(self.device)
+        self.layers = layers
+        self.input_size = input_size
         
     def forward(self, x):
+        pass
 
-        embedding_feat = self.feat_extractor(x, channel_indices=self.channel_indices, 
-                                             layer_hook=self.layer_hook, 
-                                             layer_indices=self.layer_indices)
-
-        return embedding_feat
+        
