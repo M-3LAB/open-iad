ec148d390d866af565359c5ba1d360449925202c
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-06-20 13:57:19 +0800

    update

---
+++ models/patchcore/anomaly_map.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+
+__all__ = ['AnomalyMapGenerator']
+
+class AnomalyMapGenerator:
+    def __init__(self):
+        pass
+
+    def compute_anomaly_map(self):
+        pass
+
+    def compute_anomaly_score(self):
+        pass
+
+    def __call__(self):
+        pass
