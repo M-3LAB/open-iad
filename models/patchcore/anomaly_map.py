96cedf44ac716ae43917aff9dfa42dd383c2c8de
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-06-21 14:11:51 +0800

    update

--- models/patchcore/anomaly_map.py
+++ models/patchcore/anomaly_map.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from kornia.filters import gaussian_blur2d
 
 __all__ = ['AnomalyMapGenerator']
 
@@ -8,10 +9,21 @@ class AnomalyMapGenerator:
         self.input_size = input_size
         self.sigma = sigma
 
-    def compute_anomaly_map(self):
-        pass
+    def compute_anomaly_map(self, patch_scores, feature_map_shape):
+        """Pixel level heat map
 
-    def compute_anomaly_score(self):
+        Args:
+            patch_scores (torch.Tensor): Patch level anomaly scores 
+            feature_map_shape (torch.size): 2D feature map shape 
+        
+        Returns: 
+            pixel level anomaly maps (torch.tensor)
+        """
+        height, width = feature_map_shape
+        batch_size = len(patch_scores) // (width * height)
+        return anomaly_map
+
+    def compute_anomaly_score(patch_scores):
         pass
 
     def __call__(self):
