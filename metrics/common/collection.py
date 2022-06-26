3a17ceac5e3ab9badbff557ced174bea6ae00332
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-06-26 20:42:05 +0800

    update

--- metrics/common/collection.py
+++ metrics/common/collection.py
@@ -7,6 +7,9 @@ class OpenADMetricCollection(MetricCollection):
     Wrap MetricCollction class into openad
     """
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._update_called = False
         self._threshold = 0.5
+
+    
