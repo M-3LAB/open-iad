95fc7e7ae6f6529d3d6a9cb1e0d55af7977219f0
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-07-02 11:52:40 +0800

    update

--- tools/utilize.py
+++ tools/utilize.py
@@ -18,31 +18,6 @@ __all__ = ['to_batch', 'seed_everything', 'parse_device_list', 'allocate_gpus',
            'merge_config', 'override_config', 'extract_config', 'create_folders',
            'record_path', 'save_arg', 'save_log', 'save_script', 'save_image', 'save_model']
 
-def to_batch(images, transforms, device):
-    """
-    Convert a list of numpy array images to a pytorch tensor batch with given transforms.
-    Args:
-        images: List, np.ndarray
-        transforms: torchvision T.Compose
-
-    return torch.tensor
-    """
-    assert len(images) > 0
-
-    transformed_images = []
-    for i, image in enumerate(images):
-        image = Image.fromarray(image).convert('RGB')
-        transformed_images.append(transforms(image))
-
-    height, width = transformed_images[0].shape[1:3]
-    batch = torch.zeros((len(images), 3, height, width))
-
-    for i, transformed_image in enumerate(transformed_images):
-        batch[i] = transformed_image
-
-    return batch.to(device)
-
-
 def set_grad(model, flag=True):
     for p in model.parameters():
         p.requires_grad = flag
