a690bd0dca09bf7c46b4bbb94d4ce6a888ad1d28
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-07-04 17:03:37 +0800

    update

---
+++ arch_base/cflow_ad.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+
+__all__ = ['CFlowAD']
+
+class CFlowAD():
+
+    def __init__(self):
+        pass
+
+    def train_on_epoch(self):
+        pass
+
+    def prediction(self):
+        pass
