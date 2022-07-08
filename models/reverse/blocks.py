1ce09c744be9d45127febfcc88f75e045047c513
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-07-08 14:11:03 +0800

    update

--- models/reverse/blocks.py
+++ models/reverse/blocks.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 from typing import Type, Any, Callable, Union, List, Optional
 
-__all__ = ['conv3x3', 'conv1x1', 'deconv2x2']
+__all__ = ['conv3x3', 'conv1x1', 'deconv2x2', 'AttnBasicBlock']
 
 def conv3x3(in_planes: int, out_planes: int, stride: int = 1, groups: int = 1, dilation: int = 1):
     """3x3 convolution with padding"""
@@ -18,4 +18,59 @@ def deconv2x2(in_planes: int, out_planes: int, stride: int = 1, groups: int = 1,
     """1x1 convolution"""
     return nn.ConvTranspose2d(in_planes, out_planes, kernel_size=2, stride=stride,
                               groups=groups, bias=False, dilation=dilation)
+
+class AttnBasicBlock(nn.Module):
+    expansion: int = 1
+
+    def __init__(
+        self,
+        inplanes: int,
+        planes: int,
+        stride: int = 1,
+        downsample: Optional[nn.Module] = None,
+        groups: int = 1,
+        base_width: int = 64,
+        dilation: int = 1,
+        norm_layer: Optional[Callable[..., nn.Module]] = None,
+        attention: bool = True,
+    ):
+        super(AttnBasicBlock, self).__init__()
+        self.attention = attention
+        #print("Attention:", self.attention)
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        if groups != 1 or base_width != 64:
+            raise ValueError('BasicBlock only supports groups=1 and base_width=64')
+        if dilation > 1:
+            raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
+        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = norm_layer(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = norm_layer(planes)
+        #self.cbam = GLEAM(planes, 16)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x: torch.Tensor):
+        #if self.attention:
+        #    x = self.cbam(x)
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
    
