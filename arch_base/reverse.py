b7879de22fd398d786316db7fbd601981a008aab
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-07-08 17:21:57 +0800

    update

--- arch_base/reverse.py
+++ arch_base/reverse.py
@@ -1,11 +1,12 @@
 import torch
 import torch.nn as nn
-from torchvision import models
+from models.reverse.resnet import * 
 
 __all__ = ['Reverse']
 
 class Reverse():
-    def __init__(self):
+    def __init__(self, config, train_loaders, valid_loaders,
+                 device, file):
         pass
 
     def train_epoch(self):
