5a4f8abfe2487541d1c7cd9d54199e021f7ebae2
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-07-09 10:05:58 +0800

    udpate

--- arch_base/reverse.py
+++ arch_base/reverse.py
@@ -15,6 +15,22 @@ class Reverse():
         self.file_path = file_path
         self.train_fewshot_loaders = train_fewshot_loaders
 
+        self.chosen_train_loaders = [] 
+        if self.config['chosen_train_task_ids'] is not None:
+            for idx in range(len(self.config['chosen_train_task_ids'])):
+                self.chosen_train_loaders.append(self.train_loaders[self.config['chosen_train_task_ids'][idx]])
+        else:
+            self.chosen_train_loaders = self.train_loaders
+
+        self.chosen_valid_loader = self.valid_loaders[self.config['chosen_test_task_id']] 
+
+        if self.config['fewshot']:
+            assert self.train_fewshot_loaders is not None
+            self.chosen_fewshot_loader = self.train_fewshot_loaders[self.config['chosen_test_task_id']]
+        
+        if self.config['chosen_test_task_id'] in self.config['chosen_train_task_ids']:
+            assert self.config['fewshot'] is False, 'Changeover: test task id should not be the same as train task id'
+
     def train_epoch(self):
         pass
 
