8fe7a764af9538886cbbf77343c5beced7bc1627
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-07-09 10:00:24 +0800

    update

--- arch_base/reverse.py
+++ arch_base/reverse.py
@@ -6,8 +6,14 @@ __all__ = ['Reverse']
 
 class Reverse():
     def __init__(self, config, train_loaders, valid_loaders,
-                 device, file):
-        pass
+                 device, file_path, train_fewshot_loaders=None):
+
+        self.config = config
+        self.train_loaders = train_loaders
+        self.valid_loaders = valid_loaders
+        self.device = device
+        self.file_path = file_path
+        self.train_fewshot_loaders = train_fewshot_loaders
 
     def train_epoch(self):
         pass
