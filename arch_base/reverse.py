a918bc2e411adf7d140ec2d99109b69ddbac602a
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-07-10 08:35:12 +0800

    update

--- arch_base/reverse.py
+++ arch_base/reverse.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 from models.reverse.resnet import * 
 from models.reverse.loss import *
 import numpy as np
+from torch.nn import functional as F
 
 __all__ = ['Reverse']
 
@@ -96,3 +97,32 @@ class Reverse():
         self.img_gt_list.clear()
         self.pixel_pred_list.clear()
         self.img_pred_list.clear()
+
+        with torch.no_grad():
+            for batch_id, batch in enumerate(self.chosen_valid_loader):
+                img = batch['img'].to(self.device)
+                mask = batch['mask'].to(self.device)
+                label = batch['label'].to(self.device)
+    
+    def cal_anomaly_map(self, fs_list, ft_list, out_size=224, amap_mode='full'):
+        if amap_mode == 'mul':
+            anomaly_map = np.ones([out_size, out_size])
+        else:
+            anomaly_map = np.zeros([out_size, out_size])
+
+        a_map_list = []
+        for i in range(len(ft_list)):
+            fs = fs_list[i]
+            ft = ft_list[i]
+            #fs_norm = F.normalize(fs, p=2)
+            #ft_norm = F.normalize(ft, p=2)
+            a_map = 1 - F.cosine_similarity(fs, ft)
+            a_map = torch.unsqueeze(a_map, dim=1)
+            a_map = F.interpolate(a_map, size=out_size, mode='bilinear', align_corners=True)
+            a_map = a_map[0, 0, :, :].to('cpu').detach().numpy()
+            a_map_list.append(a_map)
+            if amap_mode == 'mul':
+                anomaly_map *= a_map
+            else:
+                anomaly_map += a_map
+        return anomaly_map, a_map_list
