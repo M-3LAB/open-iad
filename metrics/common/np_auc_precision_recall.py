bdd19fa2ea30b0ce2a8aaa7eb1027b44575c8283
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-07-10 09:55:35 +0800

    update

--- metrics/common/np_auc_precision_recall.py
+++ metrics/common/np_auc_precision_recall.py
@@ -6,6 +6,7 @@ import numpy as np
 import pandas as pd
 from skimage import measure
 from numpy import ndarray
+from sklearn.metrics import auc
 
 __all__ = ['np_get_auroc', 'np_get_precision_recall', 'np_get_ap',
            'np_get_aupro']
@@ -52,4 +53,35 @@ def np_get_aupro(masks, amaps, num_th):
     assert amaps.shape == masks.shape, "amaps.shape and masks.shape must be same"
     assert set(masks.flatten()) == {0, 1}, "set(masks.flatten()) must be {0, 1}"
     assert isinstance(num_th, int), "type(num_th) must be int"
-    pass
+
+    df = pd.DataFrame([], columns=["pro", "fpr", "threshold"])
+    binary_amaps = np.zeros_like(amaps, dtype=np.bool)
+
+    min_th = amaps.min()
+    max_th = amaps.max()
+    delta = (max_th - min_th) / num_th
+
+    for th in np.arange(min_th, max_th, delta):
+        binary_amaps[amaps <= th] = 0
+        binary_amaps[amaps > th] = 1
+
+        pros = []
+        for binary_amap, mask in zip(binary_amaps, masks):
+            for region in measure.regionprops(measure.label(mask)):
+                axes0_ids = region.coords[:, 0]
+                axes1_ids = region.coords[:, 1]
+                tp_pixels = binary_amap[axes0_ids, axes1_ids].sum()
+                pros.append(tp_pixels / region.area)
+
+        inverse_masks = 1 - masks
+        fp_pixels = np.logical_and(inverse_masks, binary_amaps).sum()
+        fpr = fp_pixels / inverse_masks.sum()
+
+        df = df.append({"pro": np.mean(pros), "fpr": fpr, "threshold": th}, ignore_index=True)
+
+    # Normalize FPR from 0 ~ 1 to 0 ~ 0.3
+    df = df[df["fpr"] < 0.3]
+    df["fpr"] = df["fpr"] / df["fpr"].max()
+
+    pro_auc = auc(df["fpr"], df["pro"])
+    return pro_auc
