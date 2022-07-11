0da7d04e396d5f1a055a6cdf73083a89097a69d2
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-07-11 14:10:37 +0800

    update

--- configuration/architecture/config.py
+++ configuration/architecture/config.py
@@ -6,7 +6,9 @@ __all__ = ['parse_arguments_centralized', 'parse_arguments_federated']
 def parse_arguments_centralized():
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset', '-d', type=str, default='mvtec2d', choices=['mvtec2d', 'mvtec3d', 'mtd'])
-    parser.add_argument('--model', '-m', type=str, default='patchcore2d', choices=['patchcore2d', 'patchcore3d'])
+    parser.add_argument('--model', '-m', type=str, default='patchcore2d', choices=['patchcore2d', 'reverse',
+                                                                                   'draem', 'padim2d', 'csflow',
+                                                                                   'cflow'])
     parser.add_argument('--data-path', '-dp', type=str, default=None)
 
     parser.add_argument('--chosen-train-task-ids', type=int, default=3, nargs='+')
