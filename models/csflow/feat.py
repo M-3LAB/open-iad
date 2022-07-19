9c30a4fbd094de0c8b307976bbb55169263e7a2a
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-07-19 14:56:54 +0800

    update

--- models/csflow/feat.py
+++ models/csflow/feat.py
@@ -10,3 +10,5 @@ class FeatureExtractor(nn.Module):
 
         if self.backbone == 'efficient_net':
             self.feature_extractor = EfficientNet.from_pretrained()
+        else:
+            raise NotImplementedError('This archichitecture has not been implemented yet')
