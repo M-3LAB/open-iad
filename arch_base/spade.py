4bba3ef56f124c7bc7bca4cc168fa46f968d3588
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-07-21 15:14:28 +0800

    update

--- arch_base/spade.py
+++ arch_base/spade.py
@@ -1,2 +1,41 @@
 import torch
 import torch.nn as nn
+from torchvision import models
+
+__all__ = ['Spade']
+
+class Spade():
+    def __init__(self, config, train_loaders, valid_loaders, device, 
+                 file_path, train_fewshot_loaders=None):
+        
+        self.config = config
+        self.train_loaders = train_loaders
+        self.valid_loaders = valid_loaders
+        self.device = device
+        self.file_path = file_path
+        self.train_fewshot_loaders = train_fewshot_loaders
+
+        self.chosen_train_loaders = [] 
+        if self.config['chosen_train_task_ids'] is not None:
+            for idx in range(len(self.config['chosen_train_task_ids'])):
+                self.chosen_train_loaders.append(self.train_loaders[self.config['chosen_train_task_ids'][idx]])
+        else:
+            self.chosen_train_loaders = self.train_loaders
+
+        self.chosen_valid_loader = self.valid_loaders[self.config['chosen_test_task_id']] 
+
+        if self.config['fewshot']:
+            assert self.train_fewshot_loaders is not None
+            self.chosen_fewshot_loader = self.train_fewshot_loaders[self.config['chosen_test_task_id']]
+        
+        if self.config['chosen_test_task_id'] in self.config['chosen_train_task_ids']:
+            assert self.config['fewshot'] is False, 'Changeover: test task id should not be the same as train task id'
+
+        # Backbone model
+        if config['backbone'] == 'resnet18':
+            self.backbone = models.resnet18(pretrained=True, progress=True).to(self.device)
+        elif config['backbone'] == 'wide_resnet50':
+            self.backbone = models.wide_resnet50_2(pretrained=True, progress=True).to(self.device)
+        else:
+            raise NotImplementedError('This Pretrained Model Not Implemented Error')
+    
