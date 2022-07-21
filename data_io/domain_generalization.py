dffc81ffd7885e17dc847b6223bd610d8c346788
jinbao-wang <linkingring@163.com>
2022-07-21 17:00:00 +0800

    update

---
+++ data_io/domain_generalization.py
@@ -0,0 +1,10 @@
+import torch
+import os
+import random
+from PIL import Image
+from torchvision import transforms as T
+
+
+def domain_gen(data):
+    pass
+    return data
--- data_io/mvtec2d.py
+++ data_io/mvtec2d.py
@@ -143,7 +143,7 @@ class MVTec2DFewShot(MVTec2D):
         self.fewshot_exm = fewshot_exm
         super(MVTec2DFewShot, self).__init__(data_path=data_path, learning_mode=learning_mode, phase=phase, 
                  data_transform=data_transform, num_task=num_task)
-
+    
     def allocate_task_data(self):
         start = 0
         for num in self.sample_num_in_task:
@@ -153,3 +153,17 @@ class MVTec2DFewShot(MVTec2D):
             self.sample_indices_in_task.append(indice[:self.fewshot_exm])
             start = end
 
+class FewShot(Dataset):
+    def __init__(self, data) -> None:
+        self.data = data
+
+
+    def __getitem__(self, idx):
+        # img, label, mask, task_id = self.data[idx]['img'], self.data[idx]['label'], self.data[idx]['mask'], self.data[idx]['task_id']
+        # return {
+        #     'img': img, 'label':label, 'mask':mask, 'task_id':task_id
+        # }
+        return self.data[idx]
+
+    def __len__(self):
+        return len(self.data)
