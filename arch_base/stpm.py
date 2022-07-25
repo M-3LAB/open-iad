6f0b64623d1cf746d3b79248e5c0aedafa8b2fae
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-07-25 14:43:54 +0800

    update

--- arch_base/stpm.py
+++ arch_base/stpm.py
@@ -4,5 +4,12 @@ import torch.nn as nn
 __all__ = ['STPM']
 
 class STPM():
-    def __init__(self) -> None:
-        pass
+    def __init__(self, config, train_loaders, valid_loaders, device,
+                 file_path, train_fewshot_loaders=None):
+        
+        self.config = config
+        self.train_loader = train_loaders
+        self.valid_loader = valid_loaders
+        self.device = device
+        self.file_path = file_path
+        self.train_fewshot_loaders = train_fewshot_loaders
