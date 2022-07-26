c69943735ba763837ea9b584013d8c5638eeda2e
jinbao-wang <linkingring@163.com>
2022-07-26 16:39:13 +0800

    update

--- run_scripts/run_fewshot_normal_script.py
+++ run_scripts/run_fewshot_normal_script.py
@@ -1,13 +1,15 @@
 from cgi import test
 import os
 
-fewshots = [5]
+fewshots = [1, 2, 3, 4, 5, 7, 10]
 train_ids = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
 test_ids = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+sample_ratio = 1
 gpu_id = 7
 for fewshot_n in fewshots:
     for train_id in train_ids:
         for test_id in test_ids:
             if train_id == test_id:
-                script = 'python3 centralized_training.py --fewshot-normal --model patchcore2d --chosen-train-task-ids {} --chosen-test-task-id {} --fewshot --fewshot-exm {} -g {}'.format(train_id, test_id, fewshot_n, gpu_id)
+                script = 'python3 centralized_training.py --fewshot-normal --model patchcore2d --chosen-train-task-ids {} --chosen-test-task-id {} --fewshot-exm {} --coreset-sampling-ratio {} -g {}'.format(
+                    train_id, test_id, fewshot_n, sample_ratio, gpu_id)
                 os.system(script)
