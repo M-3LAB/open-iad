8408a22acc4db25e1f972528abe1792f1717b52b
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-07-28 16:27:17 +0800

    stpm finished and pfm begin

--- arch_base/pfm.py
+++ arch_base/pfm.py
@@ -5,8 +5,15 @@ __all__ = ['PFM']
 
 class PFM():
 
-    def __init__(self):
-        pass
+    def __init__(self, config, train_loaders, valid_loaders, device, 
+                 file_path, train_fewshot_loaders=None):
+    
+        self.config = config
+        self.train_loaders = train_loaders
+        self.valid_loaders = valid_loaders
+        self.device = device
+        self.file_path = file_path
+        self.train_fewshot_loaders = train_fewshot_loaders
 
     def train_on_epoch(self):
         pass
