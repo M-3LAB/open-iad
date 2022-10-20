324689f74249e674792feeb9ae408ebbf99b1f7b
jinbao-wang <linkingring@162.com>
2022-10-20 14:42:10 +0800

    unified dataset loading

--- configuration/architecture/config.py
+++ configuration/architecture/config.py
@@ -28,7 +28,7 @@ def assign_service():
 
 def parse_arguments_centralized():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dataset', '-d', type=str, default='mvtec2df3d', choices=['mvtec2d', 'mvtec3d', 'mpdd', 'mvtecloco', 'mtd', 'btad', 'mvtec2df3d'])
+    parser.add_argument('--dataset', '-d', type=str, default='mvtec2d', choices=['mvtec2d', 'mvtec3d', 'mpdd', 'mvtecloco', 'mtd', 'btad', 'mvtec2df3d'])
     parser.add_argument('--model', '-m', type=str, default='patchcore2d', choices=['patchcore2d'])
     parser.add_argument('--root-path', '-rp', type=str, default=None)
     parser.add_argument('--data-path', '-dp', type=str, default=None)
@@ -44,7 +44,7 @@ def parse_arguments_centralized():
     parser.add_argument('--continual', '-c', action='store_true', default=False)
 
     # fewshot learniing
-    parser.add_argument('--fewshot', '-f', action='store_true', default=False)
+    parser.add_argument('--fewshot', '-f', action='store_true', default=True)
     parser.add_argument('--fewshot-exm', '-fe', type=int, default=1)
     parser.add_argument('--fewshot-data-aug', '-fda', action='store_true', default=False)
     parser.add_argument('--fewshot-feat-aug', '-ffa', action='store_true', default=False)
