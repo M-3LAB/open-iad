c049dac08f4d03ac3e6707b1ded7388924db31da
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-10-21 15:46:52 +0800

    add cutpaste augmentation

--- data_io/augmentation/cutpaste_augmentation.py
+++ data_io/augmentation/cutpaste_augmentation.py
@@ -1,6 +1,9 @@
 import torch
 import torch.nn as nn
 from torchvision import transforms
+import random
+
+__all__ = ['CutPaste', 'CutPasteNormal', 'CutPasteScar']
 
 class CutPaste(object):
     """Base class for both cutpaste variants with common operations"""
@@ -25,3 +28,101 @@ class CutPaste(object):
             # org_img = Image.fromarray(org_img)
             org_img = self.transform(org_img)
         return org_img, img
+
+class CutPasteNormal(CutPaste):
+    """Randomly copy one patche from the image and paste it somewere else.
+    Args:
+        area_ratio (list): list with 2 floats for maximum and minimum area to cut out
+        aspect_ratio (float): minimum area ration. Ration is sampled between aspect_ratio and 1/aspect_ratio.
+    """
+
+    def __init__(self, area_ratio=[0.02, 0.15], aspect_ratio=0.3, **kwags):
+        super(CutPasteNormal, self).__init__(**kwags)
+        self.area_ratio = area_ratio
+        self.aspect_ratio = aspect_ratio
+
+    def __call__(self, img):
+        # TODO: we might want to use the pytorch implementation to calculate the patches from https://pytorch.org/vision/stable/_modules/torchvision/transforms/transforms.html#RandomErasing
+        h = img.size[0]
+        w = img.size[1]
+
+        new_aspect_ratio = random.uniform(0.1, 1)
+
+        augmented = img.copy()
+        # ratio between area_ratio[0] and area_ratio[1]
+        ratio_area = random.uniform(self.area_ratio[0], self.area_ratio[1]) * w * h
+
+        # sample in log space
+        log_ratio = torch.log(torch.tensor((new_aspect_ratio, 1 / new_aspect_ratio)))
+        aspect = torch.exp(
+            torch.empty(1).uniform_(log_ratio[0], log_ratio[1])
+        ).item()
+
+        cut_w = int(round(math.sqrt(ratio_area * aspect)))
+        cut_h = int(round(math.sqrt(ratio_area / aspect)))
+
+        # one might also want to sample from other images. currently we only sample from the image itself
+        from_location_h = int(random.uniform(0, h - cut_h))
+        from_location_w = int(random.uniform(0, w - cut_w))
+
+        box = [from_location_w, from_location_h, from_location_w + cut_w, from_location_h + cut_h]
+        patch = img.crop(box)
+
+        # Before pasting, we apply color jitter.we rotate or jitter pixel values in the patch
+        if self.colorJitter:
+            patch = self.colorJitter(patch)
+
+        to_location_h = int(random.uniform(0, h - cut_h))
+        to_location_w = int(random.uniform(0, w - cut_w))
+
+        insert_box = [to_location_w, to_location_h, to_location_w + cut_w, to_location_h + cut_h]
+
+        augmented.paste(patch, insert_box)  # The pasted image patch always origins from the same image it is pasted to
+
+        return super().__call__(img, augmented)
+
+class CutPasteScar(CutPaste):
+    """Randomly copy one patche from the image and paste it somewere else.
+    Args:
+        width (list): width to sample from. List of [min, max]
+        height (list): height to sample from. List of [min, max]
+        rotation (list): rotation to sample from. List of [min, max]
+    """
+    def __init__(self, width=[2,16], height=[10,25], rotation=[-45,45], **kwags):
+        super(CutPasteScar, self).__init__(**kwags)
+        self.width = width
+        self.height = height
+        self.rotation = rotation
+    
+    def __call__(self, img):
+        h = img.size[0]
+        w = img.size[1]
+        
+        # cut region
+        cut_w = random.uniform(*self.width)
+        cut_h = random.uniform(*self.height)
+        
+        from_location_h = int(random.uniform(0, h - cut_h))
+        from_location_w = int(random.uniform(0, w - cut_w))
+        
+        box = [from_location_w, from_location_h, from_location_w + cut_w, from_location_h + cut_h]
+        patch = img.crop(box)
+        
+        if self.colorJitter:
+            patch = self.colorJitter(patch)
+
+        # rotate
+        rot_deg = random.uniform(*self.rotation)
+        patch = patch.convert("RGBA").rotate(rot_deg,expand=True)
+        
+        #paste
+        to_location_h = int(random.uniform(0, h - patch.size[0]))
+        to_location_w = int(random.uniform(0, w - patch.size[1]))
+
+        mask = patch.split()[-1]
+        patch = patch.convert("RGB")
+        
+        augmented = img.copy()
+        augmented.paste(patch, (to_location_w, to_location_h), mask=mask)
+        
+        return super().__call__(img, augmented)
