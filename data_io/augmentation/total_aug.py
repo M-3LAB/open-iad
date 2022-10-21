e335bf81a132b2d37fdeba3af290d48a5be564c7
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-10-21 22:01:10 +0800

    update

--- data_io/augmentation/total_aug.py
+++ data_io/augmentation/total_aug.py
@@ -5,13 +5,20 @@ __all__ = ['total_aug']
 
 def total_aug(args):
     if args['augment_type'] == 'normal':
-        imge_transform = T.Compose([T.Resize((self.data_transform['data_size'], self.data_transform['data_size'])),
-                                    T.CenterCrop(self.data_transform['data_crop_size']),
+        img_transform = T.Compose([T.Resize((args['data_size'], args['data_size'])),
+                                    T.CenterCrop(args['data_crop_size']),
                                     T.ToTensor(),
                                     T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
                                     ])
 
-        mask_transform = T.Compose([T.Resize(self.data_transform['mask_size']),
-                                        T.CenterCrop(self.data_transform['mask_crop_size']),
-                                        T.ToTensor(),
-                                        ])
+        mask_transform = T.Compose([T.Resize(args['mask_size']),
+                                    T.CenterCrop(args['mask_crop_size']),
+                                    T.ToTensor(),
+                                    ])
+    
+    elif args['augment_type'] == 'cutpaste':
+        #after_cutpaste_transform = 
+        pass
+    
+    
+    return img_transform, mask_transform
