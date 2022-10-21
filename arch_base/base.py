83978760a508ba0b236560326fe0406d9376b5ea
jinbao-wang <linkingring@162.com>
2022-10-21 14:11:39 +0800

    add arch base

--- arch_base/base.py
+++ arch_base/base.py
@@ -1,5 +1,12 @@
-
 class Base():
-    def __init__(self, config, train_loader, valid_loader,
-                 device, file_path, batch_limit_weight=1.0):
+    def __init__(self, config, device, file_path):
+        self.config = config
+        self.device = device
+        self.file_path = file_path
+    
+    def train_epoch(self, train_loaders, inf=''):
+        pass
+
+
+    def prediction(self, valid_loader):
         pass
