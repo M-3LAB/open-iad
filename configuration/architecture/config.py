3f7fb7cdc80e0aad65dd2637d87314c7c5c47bd4
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-10-23 09:57:02 +0800

    update

--- configuration/architecture/config.py
+++ configuration/architecture/config.py
@@ -34,7 +34,7 @@ def assign_service(guoyang):
 def parse_arguments_centralized():
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset', '-d', type=str, default='mvtec2d', choices=['mvtec2d', 'mvtec3d', 'mpdd', 'mvtecloco', 'mtd', 'btad', 'mvtec2df3d'])
-    parser.add_argument('--model', '-m', type=str, default='csflow', choices=['patchcore2d', 'csflow'])
+    parser.add_argument('--model', '-m', type=str, default='csflow', choices=['patchcore2d', 'csflow', 'igd'])
     parser.add_argument('--net', '-n', type=str, default='net_csflow', choices=['wide_resnet50', 'resnet18', 'net_csflow'])
     parser.add_argument('--root-path', '-rp', type=str, default=None)
     parser.add_argument('--data-path', '-dp', type=str, default=None)
