0f4391e1a07e87af869da959d4fde335b4cc8ab7
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-10-23 12:38:35 +0800

    add igd

--- arch_base/igd.py
+++ arch_base/igd.py
@@ -14,11 +14,22 @@ class IGD():
         self.generator = self.net['g'].to(self.device)
         self.discriminator = self.net['d'].to(self.device)
 
+    def init_c(self):
+        pass
+
+    def init_sigma(self):
+        pass
     
     def train_model(self, train_loaders, inf=''):
+        self.generator.train()
+        self.discriminator.train()
+
         for param in self.generator.pretrain.parameters():
             param.requires_grad = False
+        
+        
             
 
     def prediction(self, valid_loader):
-        pass
+        self.generator.eval()
+        self.discriminator.eval()
