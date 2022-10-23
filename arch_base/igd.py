f71d6ef3c9633ff7379a16a3ce65a1428e63e8da
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-10-23 09:21:13 +0800

    add igd

--- arch_base/igd.py
+++ arch_base/igd.py
@@ -1,2 +1,13 @@
 import torch
 import torch.nn as nn
+
+class IGD():
+    def __init__(self, config, device, file_path, net, optimizer, scheduler):
+        self.config = config
+        self.device = device
+        self.file_path = file_path
+        self.net = net
+    
+    
+    def train_model(self, train_loaders, inf=''):
+        pass
