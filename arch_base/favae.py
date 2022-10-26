5c4df3a313ddcc8743cea5f38a197c416219b289
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-10-26 16:24:12 +0800

    update

--- arch_base/favae.py
+++ arch_base/favae.py
@@ -7,6 +7,14 @@ __all__ = ['FAVAE']
 class FAVAE(ModelBase):
     def __init__(self, config, device, file_path, net, optimizer, scheduler):
         self.config = config
+        self.device = device
+        self.file_path = file_path
+        self.net = net
+        self.optimizer = optimizer
+        self.scheduler = scheduler
     
     def train_model(self, train_loaders, inf=''):
+        pass
+
+    def prediction(self, valid_loader, task_id=None):
         pass
