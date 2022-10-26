3da44bbc4d04606c78797ea0b5fda6e26d38a454
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-10-26 09:27:06 +0800

    devnet update

--- arch_base/devnet.py
+++ arch_base/devnet.py
@@ -1,2 +1,20 @@
+from arch_base.base import ModelBase
 import torch
 import torch.nn as nn
+from arch_base.base import ModelBase
+
+__all__ = ['DevNet']
+
+class DevNet(ModelBase):
+    def __init__(self, config, device, file_path, net, optimizer, scheduler):
+        self.config = config
+        self.device = device
+        self.file_path = file_path
+        self.net = net
+        self.optimizer = optimizer
+    
+    def train_model(self, train_loaders, inf=''):
+        pass
+
+    def prediction(self, valid_loder, task_id=None):
+        pass
