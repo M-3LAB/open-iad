7ded26f0a8d6a56a664c9f900d9f560a9218d6a0
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-10-27 07:46:15 +0800

    udpate

--- architecture/centralized/centralized_learning.py
+++ architecture/centralized/centralized_learning.py
@@ -258,7 +258,8 @@ class CentralizedTrain():
             self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=args._step_size, gamma=args._gamma)       
         if self.para_dict['model'] == 'favae':
             self.net = VAE(input_channel=self.para_dict['input_channel'], z_dim=100) 
-            self.opimizer =  get_optimizer(args.self.net)
+            self.opimizer =  get_optimizer(args, self.net)
+            self.scheduler = None
 
         model_name = {'patchcore2d': ('arch_base.patchcore2d', 'patchcore2d', 'PatchCore2D'),
                       'csflow': ('arch_base.csflow', 'csflow', 'CSFlow'),
@@ -266,7 +267,8 @@ class CentralizedTrain():
                       'draem': ('arch_base.draem', 'draem', 'DRAEM'),
                       'igd': ('arch_base.igd', 'igd', 'IGD'),
                       'dra': ('arch_base.dra', 'dra', 'DRA'),
-                      'devnet': ('arch_base.devnet', 'devnet', 'DevNet')
+                      'devnet': ('arch_base.devnet', 'devnet', 'DevNet'),
+                      'favae': ('arch_base.favae', 'favae', 'FAVAE')
                      }
 
         model_package = __import__(model_name[self.para_dict['model']][0])
