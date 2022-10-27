ef36a6f1e3bc4fd65a879994022f0c19ede0b16c
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-10-27 14:31:16 +0800

    update

--- arch_base/favae.py
+++ arch_base/favae.py
@@ -32,8 +32,21 @@ class FAVAE(ModelBase):
                 # get model's intermediate outputs
                 s_activations, _ = feature_extractor(z, self.net.decode, target_layers=['10', '16', '22'])
                 t_activations, _ = feature_extractor(z, self.teacher.features, target_layers=['7', '14', '21'])
-                
+
                 self.optimizer.zero_grad()
+                mse_loss = self.mse_criterion(output, img)
+                kld_loss = 0.5 * torch.sum(-1 - log_var + torch.exp(log_var) + mu**2)
+                for i in range(len(s_activations)):
+                    s_act = self.net.adapter[i](s_activations[-(i + 1)])
+                    mse_loss += self.mse_criterion(s_act, t_activations[i])
+                loss = mse_loss + self.config['kld_weight'] * kld_loss
+                losses.update(loss.sum().item(), img.size(0))
+
+                loss.backward()
+                self.optimizer.step()
+
+
+                
 
     def prediction(self, valid_loader, task_id=None):
         pass
