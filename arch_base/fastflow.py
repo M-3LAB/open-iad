c230b02e3356802d00c9f1c53e5108e416ab3286
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-10-28 12:48:40 +0800

    update

--- arch_base/fastflow.py
+++ arch_base/fastflow.py
@@ -1,4 +1,20 @@
 import torch
 import torch.nn as nn
+from arch_base.base import ModelBase
 
 __all__ = ['FastFlow']
+
+class FastFlow(ModelBase):
+    def __init__(self, config, device, file_path, net, optimizer, scheduler):
+        self.config = config
+        self.device = device
+        self.file_path = file_path
+        self.net = net
+        self.optimizer = optimizer
+        self.scheduler = scheduler
+
+    def train_model(self, train_loader, inf=''):
+        pass
+    
+    def prediction(self, valid_loader, task_id=None):
+        pass
