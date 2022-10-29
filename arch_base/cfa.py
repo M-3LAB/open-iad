25501f57fdbb23ce3d58b12f7bfdb54017f0cc02
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-10-29 14:33:55 +0800

    fixing cfa bugs

--- arch_base/cfa.py
+++ arch_base/cfa.py
@@ -14,32 +14,15 @@ __all__ = ['CFA']
 
 class CFA():
 
-    def __init__(self, config, train_loaders, valid_loaders, device, 
-                 file_path, train_fewshot_loaders=None):
+    def __init__(self, config, device, file_path, net, optimizer):
     
         self.config = config
-        self.train_loaders = train_loaders
-        self.valid_loaders = valid_loaders
         self.device = device
         self.file_path = file_path
-        self.train_fewshot_loaders = train_fewshot_loaders
+        self.net = net
+        self.net.to(self.device)
+        self.optimizer = optimizer
 
-        self.chosen_train_loaders = [] 
-        if self.config['chosen_train_task_ids'] is not None:
-            for idx in range(len(self.config['chosen_train_task_ids'])):
-                self.chosen_train_loaders.append(self.train_loaders[self.config['chosen_train_task_ids'][idx]])
-        else:
-            self.chosen_train_loaders = self.train_loaders
-
-        self.chosen_valid_loader = self.valid_loaders[self.config['chosen_test_task_id']] 
-
-        if self.config['fewshot']:
-            assert self.train_fewshot_loaders is not None
-            self.chosen_fewshot_loader = self.train_fewshot_loaders[self.config['chosen_test_task_id']]
-        
-        if self.config['chosen_test_task_id'] in self.config['chosen_train_task_ids']:
-            assert self.config['fewshot'] is False, 'Changeover: test task id should not be the same as train task id'
-        
         if self.config['backbone'] == 'resnet18':
             self.backbone = resnet18(pretrained=True, progress=True)
         elif self.config['backbone'] == 'efficientnet':
@@ -49,12 +32,7 @@ class CFA():
         elif self.config['backbone'] == 'vgg':
             self.backbone = vgg19(pretrained=True, progress=True)
 
-        self.loss_fn = DSVDD(model=self.backbone, data_loader=self.chosen_train_loaders,
-                        cnn=self.config['backbone'], gamma_c=self.config['gamma_c'],
-                        gamma_d=self.config['gamma_d'], device=self.device)
-
-        self.loss_fn = self.loss_fn.to(self.device)
-
+        
         self.pixel_gt_list = []
         self.img_gt_list = []
         self.pixel_pred_list = []
@@ -107,10 +85,13 @@ class CFA():
         #return fpr, tpr, per_pixel_rocauc
         return per_pixel_rocauc
 
-        
-        
+    def train_model(self, train_loader, task_id, inf=''):
 
-    def train_on_epoch(self):
+        self.loss_fn = DSVDD(model=self.backbone, data_loader=train_loader,
+                             cnn=self.config['backbone'], gamma_c=self.config['gamma_c'],
+                             gamma_d=self.config['gamma_d'], device=self.device)
+
+        self.loss_fn = self.loss_fn.to(self.device)
 
         self.backbone.eval()
 
@@ -166,7 +147,7 @@ class CFA():
                 print('[%d / %d]image ROCAUC: %.3f | best: %.3f'% (epoch, self.config['num_epochs'], img_auroc, self.best_img_auroc))
                 print('[%d / %d]pixel ROCAUC: %.3f | best: %.3f'% (epoch, self.config['num_epochs'], pixel_auroc, self.best_pixel_auroc))
 
-    def prediction(self):
+    def prediction(self, valid_loader, task_id=None):
         self.loss_fn.eval()
         self.pixel_gt_list.clear()
         self.img_gt_list.clear()
