48c4a29758cc466c85cbb483b90e49f9ad5997d9
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-10-30 19:39:47 +0800

    update

--- data_io/mvtec2d.py
+++ data_io/mvtec2d.py
@@ -51,8 +51,11 @@ class MVTec2D(Dataset):
         img = self.img_transform(img)
 
         if label == 0:
-            #mask = torch.zeros([1, img.shape[1], img.shape[2]])
-            mask = torch.zeros([1, 224, 224])
+            if isinstance(img, tuple):
+                mask = torch.zeros([1, img[0].shape[1], img[0].shape[2]])
+            else:
+                mask = torch.zeros([1, img.shape[1], img.shape[2]])
+            #mask = torch.zeros([1, 224, 224])
         else:
             mask = Image.open(mask)
             mask = self.mask_transform(mask)
