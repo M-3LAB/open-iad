b5154427d7dc05988a787fd48c8142d929e044bf
shirowalker <985797670@qq.com>
2022-10-30 21:35:53 +0800

    update fastflow

--- arch_base/fastflow.py
+++ arch_base/fastflow.py
@@ -1,12 +1,14 @@
 import torch
 from arch_base.base import ModelBase
 from models.fastflow.func import AverageMeter
-from ignite.contrib import metrics
+import numpy as np
+# from ignite.contrib import metrics
 
 __all__ = ['FastFlow']
 
 class FastFlow(ModelBase):
     def __init__(self, config, device, file_path, net, optimizer, scheduler):
+        super(FastFlow, self).__init__(config, device, file_path, net, optimizer, scheduler)
         self.config = config
         self.device = device
         self.file_path = file_path
@@ -17,6 +19,7 @@ class FastFlow(ModelBase):
 
     def train_model(self, train_loader, inf=''):
         self.net.train()
+        self.clear_all_list()
         loss_meter = AverageMeter() 
         for epoch in range(self.config['num_epochs']):
             for batch_id, batch in enumerate(train_loader):
@@ -35,23 +38,27 @@ class FastFlow(ModelBase):
     
     def prediction(self, valid_loader, task_id=None):
         self.net.eval()
-        auroc_metric = metrics.ROC_AUC()
+        self.clear_all_list()
+        # auroc_metric = metrics.ROC_AUC()
         with torch.no_grad():
             for batch_id, batch in enumerate(valid_loader):
                 img = batch['img'].to(self.device)
-                mask = batch['mask'].to(self.device)
+                mask = batch['mask']
+                label = batch['label']
                 mask[mask>=0.5] = 1
                 mask[mask<0.5] = 0
+                mask_np = mask.numpy()[0,0].astype(int)
 
                 ret = self.net(img)
 
-                outputs = ret["anomaly_map"].cpu().detach()
-                outputs = outputs.flatten()
-                mask = mask.flatten()
-                auroc_metric.update((outputs, mask))
-        
-        pixel_auroc = auroc_metric.compute()
-        img_auroc = 0 
-        return pixel_auroc, img_auroc 
+                outputs = ret["anomaly_map"].cpu().detach().numpy()
+                # outputs = outputs.flatten()
+                # mask = mask.flatten()
+                # auroc_metric.update((outputs, mask))
+                self.pixel_gt_list.append(mask_np)
+                self.pixel_pred_list.append(outputs[0,0,:,:])
+                self.img_gt_list.append(label.numpy()[0])
+                self.img_pred_list.append(np.max(outputs))
+                self.img_path_list.append(batch['img_src'])
         
     
