9f702c40aa6572616d75120ebbd43115e5f28d2d
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2023-03-13 13:46:06 +0800

    update

--- models/graphcore/graph_lib/graphcore.py
+++ models/graphcore/graph_lib/graphcore.py
@@ -1,8 +1,14 @@
 import torch
 import torch.nn as nn
 
+
+__all__ = ['GraphCore']
+
 class GraphCore(nn.Module):
 
     def __init__(self, config):
         super().__init__()
         self.config = config
+    
+    def forward(self, x):
+        pass
