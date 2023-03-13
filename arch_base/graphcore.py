d2d2daee0001ea23f7333cb372b7fb1728ac5eb1
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2023-03-13 16:57:02 +0800

    add vig

---
+++ arch_base/graphcore.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+from arch_base.base import ModelBase
+
+
+__all__ = ['GraphCore']
+
+class GraphCore(ModelBase):
+
+    def __init__(self, config, device, file_path, net, optimizer, scheduler):
+        super().__init__(config, device)
+
+        self.config = config
+        self.device = device
+        self.net = net
+        self.optimizer = optimizer
+        self.scheduler = scheduler
+    
+    def forward(self, x):
+        pass
