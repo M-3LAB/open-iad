6849908f01c9e1fb43845303cea45df525eb8ce8
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2023-03-15 08:43:27 +0800

    update

--- models/graphcore/net_graphcore.py
+++ models/graphcore/net_graphcore.py
@@ -1,8 +1,15 @@
 import torch
 import torch.nn as nn
+from models.graphcore.pyramid_vig import *
+from models.graphcore.vig import *
 
 __all__ = ['NetGraphCore']
 
 class NetGraphCore(nn.module):
-    def __init__(self):
+    def __init__(self, config):
         super().__init__()
+        self.config = config
+
+    
+    #def forward(self, x):
+    #    pass
