7402f0c989e4c21c8dfddf3af1977ee0d58fdc10
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2023-03-16 16:01:00 +0800

    update

--- configuration/config.py
+++ configuration/config.py
@@ -13,7 +13,7 @@ def parse_arguments_main():
     parser.add_argument('--dataset', '-d', type=str, default='mvtec2d', choices=['mvtec2d', 'mvtec3d', 'mpdd', 'mvtecloco', 'mtd', 'btad', 'mvtec2df3d', 'visa', 'dagm', 'imad_hardware_parts'])
 
     parser.add_argument('--model', '-m', type=str, default='patchcore', choices=['_patchcore', 'patchcore', 'csflow', 'dne', 'draem', 'igd', 'cutpaste', 'devnet', 'dra', 
-                                                                              'favae', 'padim', 'reverse', 'spade', 'fastflow', 'softpatch', 'cfa', 'stpm'])
+                                                                              'favae', 'padim', 'reverse', 'spade', 'fastflow', 'softpatch', 'cfa', 'stpm', 'graphcore'])
     parser.add_argument('--net', '-n', type=str, default='resnet18', choices=['wide_resnet50', 'resnet18', 'net_csflow', 'vit_b_16', 'net_draem', 'net_dra',
                                                                               'net_igd', 'net_reverse', 'net_favae', 'net_fastflow', 'net_cfa', 'net_devnet'])
     parser.add_argument('--root-path', '-rp', type=str, default=None)
