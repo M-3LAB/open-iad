6e40b3fb1395db73a6931841fc19a85e01728daa
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2023-03-17 15:24:24 +0800

    update

--- arch_base/graphcore.py
+++ arch_base/graphcore.py
@@ -1,7 +1,15 @@
 import torch
-import torch.nn as nn
+from models._patchcore.kcenter_greedy import KCenterGreedy 
+import cv2
+import torch.nn.functional as F
+import numpy as np
+from sklearn.random_projection import SparseRandomProjection
+import faiss
+import math
+from scipy.ndimage import gaussian_filter
 from arch_base.base import ModelBase
-
+from tools.utils import *
+import os
 
 __all__ = ['GraphCore']
 
@@ -15,8 +23,19 @@ class GraphCore(ModelBase):
         self.file_path = file_path
         self.net = net
         self.model = self.net.model
+        self.model.to(self.device)
+
+        self.features = []
+        self.random_projector = SparseRandomProjection(n_components='auto', eps=0.9)
+        self.embedding_coreset = np.array([])
+        self.embedding_path = self.file_path + '/embed'
+        create_folders(self.embedding_path)
+
         self.optimizer = optimizer
         self.scheduler = scheduler
+    
+    def get_layer_features(self):
+        pass
     
     def train_model(self, train_loader, task_id, inf=''):
         pass
