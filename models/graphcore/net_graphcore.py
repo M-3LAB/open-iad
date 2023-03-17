dffd9bd612aa9c741136c02ad2a3982948e268eb
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2023-03-17 11:21:58 +0800

    solve model loading bugs

--- models/graphcore/net_graphcore.py
+++ models/graphcore/net_graphcore.py
@@ -13,24 +13,23 @@ class NetGraphCore(nn.Module):
         super().__init__()
         self.config = config
         create_folders(self.config.checkpoint_path)
-        create_folders(self.config.pretrain_path)
 
         self.model = create_model(self.config.net,
                                   pretrained=self.config.pretrained,
                                   num_classes=1000,
                                   drop_rate=self.config.drop_rate,
-                                  drop_path_rate=self.drop_path_rate,
-                                  drop_block_rate=self.drop_block,
+                                  drop_path_rate=self.config.drop_path_rate,
+                                  drop_block_rate=self.config.drop_block_rate,
                                   global_pool=self.config.gp,
-                                  bn_tf=self.bn_tf,
-                                  bn_momentum=self.bn_momentum,
-                                  bn_eps=self.bn_eps,
-                                  checkpoint_path=self.checkpoint_path)
-
+                                  bn_tf=self.config.bn_tf,
+                                  bn_momentum=self.config.bn_momentum,
+                                  bn_eps=self.config.bn_eps)
+        #print('test')
         # Loading pretrained model  
-        if self.config.pretrain_path is not None:
-            print('Loading:', self.config.pretrain_path)
-            state_dict = torch.load(self.config.pretrain_path)
+        if self.config.checkpoint_path is not None:
+            print('Loading:', self.config.checkpoint_path)
+            #state_dict = torch.load(self.config.checkpoint_path)
+            state_dict = torch.load(self.config.checkpoint_path)
             self.model.load_state_dict(state_dict, strict=False)
             print('Pretrain weights loaded')
         
