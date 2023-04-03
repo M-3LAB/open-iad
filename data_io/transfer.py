3f30b158a5789e5a06dbea12c969a78506155afe
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2023-04-03 09:08:19 +0800

    update

--- data_io/transfer.py
+++ data_io/transfer.py
@@ -3,5 +3,12 @@ import random
 
 __all__ = ['extract_transfer_data']
 
-def extract_transfer_data():
-    pass
+def extract_transfer_data(source_domain_dataset, target_domain_dataset, transfer_type):
+    source_sample_nums = [0] + source_domain_dataset.sample_num_in_task
+    source_sample_indice = source_domain_dataset.sample_indices_in_task
+
+    target_sample_nums = [0] + target_domain_dataset.sample_num_in_task
+    target_sample_indice = target_domain_dataset.sample_indices_in_task
+
+    if transfer_type == 'inter_class':
+        pass
