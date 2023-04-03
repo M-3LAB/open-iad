e9516c62a09fe0dbacc93217837465e5cf1482ba
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2023-04-03 15:31:39 +0800

    update

--- data_io/transfer.py
+++ data_io/transfer.py
@@ -19,6 +19,14 @@ def extract_transfer_data(source_dataset, target_dataset, transfer_type='inter_c
     else:
         raise NotImplementedError('Transfer Type Has Not Been Implemented')
 
+    # source normal training dataset 
+
+    # target normal training dataset 
+
+    # source anomaly training dataset 
+
+    # target anomaly training dataset
+
     # obtain target number dataset  
     target_train_dataset = copy.deepcopy(target_dataset)
     for i, num in enumerate(target_dataset.sample_num_in_task): 
