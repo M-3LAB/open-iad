492e7f3443457691b98d5f9784cde1dfcdc92422
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2023-04-04 09:24:41 +0800

    add the interface of api

--- data_io/transfer.py
+++ data_io/transfer.py
@@ -3,14 +3,26 @@ import random
 
 __all__ = ['extract_transfer_data']
 
-def extract_transfer_data(source_train_dataset, target_dataset, transfer_type='inter_class', 
-                          target_train_num=2):
+def extract_transfer_data(
+                          source_train_dataset, 
+                          source_valid_dataset, 
+                          target_train_dataset, 
+                          target_valid_dataset,
+                          transfer_type='inter_class', 
+                          target_train_num=2
+                          ):
 
-    source_sample_nums = [0] + source_dataset.sample_num_in_task
-    source_sample_indice = source_dataset.sample_indices_in_task
+    source_train_sample_nums = [0] + source_train_dataset.sample_num_in_task
+    source_train_sample_indice = source_train_dataset.sample_indices_in_task
 
-    target_sample_nums = [0] + target_dataset.sample_num_in_task
-    target_sample_indice = target_dataset.sample_indices_in_task
+    source_valid_sample_nums = [0] + source_valid_dataset.sample_num_in_task
+    source_valid_sample_indice = source_valid_dataset.sample_indices_in_task
+
+    target_train_sample_nums = [0] + target_train_dataset.sample_num_in_task
+    target_train_sample_indice = target_train_dataset.sample_indices_in_task
+
+    target_valid_sample_nums = [0] + target_valid_dataset.sample_num_in_task
+    target_valid_sample_indice = target_valid_dataset.sample_indices_in_task
 
     if transfer_type == 'inter_class':
         pass
