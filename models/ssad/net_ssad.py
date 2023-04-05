c74d4d310245fdf8cdff35b19de728487736cad6
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2023-04-05 10:02:59 +0800

    update

---
+++ models/ssad/net_ssad.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+
+__all__ = ['NetSSAD']
+
+class NetSSAD(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+    
+    def forward(self, x):
+        pass
