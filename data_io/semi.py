68ee2deaca41d8bb4f9b8fb3bd04abe11b3587d1
jinbao-wang <linkingring@163.com>
2023-04-24 14:40:29 +0800

    noisy -> semi

--- data_io/semi.py
+++ data_io/semi.py
@@ -8,8 +8,8 @@ def extract_semi_data(train_dataset, valid_dataset, anomaly_num=10, anomaly_over
     valid_sample_nums = [0] + valid_dataset.sample_num_in_task
     valid_sample_indice = valid_dataset.sample_indices_in_task
 
-    # obtain noisy label in validation set
-    noisy_indices = []
+    # obtain semi label in validation set
+    semi_indices = []
     for i in range(valid_dataset.num_task):
         anomaly_index = []
         for k, j in enumerate(range(valid_sample_nums[i], valid_sample_nums[i] + valid_sample_nums[i+1])):
@@ -17,43 +17,43 @@ def extract_semi_data(train_dataset, valid_dataset, anomaly_num=10, anomaly_over
             label = valid_dataset.labels_list[z]
             if label == 1:
                 anomaly_index.append(valid_sample_indice[i][k])
-        # set noisy data to be less than 50 percent of these in test set
+        # set semi data to be less than 50 percent of these in test set
         anomaly_num_max = int(len(anomaly_index) * upper_ratio)
         if anomaly_num >= anomaly_num_max:
             anomaly_num = anomaly_num_max
-        noise_index = random.sample(anomaly_index, anomaly_num)
+        semi_index = random.sample(anomaly_index, anomaly_num)
 
-        noisy_indices.append(noise_index)
+        semi_indices.append(semi_index)
 
-    # construct valid_noisy_dataset
-    valid_noisy_dataset = copy.deepcopy(valid_dataset)
+    # construct valid_semi_dataset
+    valid_semi_dataset = copy.deepcopy(valid_dataset)
     if not anomaly_overlap:
         for task_id in range(valid_dataset.num_task):
-            valid_noisy_dataset.sample_indices_in_task[task_id] = list(set(valid_sample_indice[task_id]) - set(noisy_indices[task_id]))
-            valid_noisy_dataset.sample_num_in_task[task_id] = len(valid_noisy_dataset.sample_indices_in_task[task_id])
+            valid_semi_dataset.sample_indices_in_task[task_id] = list(set(valid_sample_indice[task_id]) - set(semi_indices[task_id]))
+            valid_semi_dataset.sample_num_in_task[task_id] = len(valid_semi_dataset.sample_indices_in_task[task_id])
 
-    # construct noisy_dataset 
-    noisy_dataset = copy.deepcopy(valid_dataset)
+    # construct semi_dataset 
+    semi_dataset = copy.deepcopy(valid_dataset)
     for task_id in range(valid_dataset.num_task):
-        noisy_dataset.sample_indices_in_task[task_id] = noisy_indices[task_id] 
-        noisy_dataset.sample_num_in_task[task_id] = len(noisy_dataset.sample_indices_in_task[task_id])
+        semi_dataset.sample_indices_in_task[task_id] = semi_indices[task_id] 
+        semi_dataset.sample_num_in_task[task_id] = len(semi_dataset.sample_indices_in_task[task_id])
 
-    # construct train_noisy_dataset
-    train_noisy_dataset = copy.deepcopy(train_dataset)
+    # construct train_semi_dataset
+    train_semi_dataset = copy.deepcopy(train_dataset)
     for task_id in range(valid_dataset.num_task):
-        for img_id in noisy_indices[task_id]:
-            train_noisy_dataset.imgs_list.append(noisy_dataset.imgs_list[img_id]) 
-            train_noisy_dataset.labels_list.append(noisy_dataset.labels_list[img_id]) 
-            train_noisy_dataset.masks_list.append(noisy_dataset.masks_list[img_id]) 
-            train_noisy_dataset.task_ids_list.append(noisy_dataset.task_ids_list[img_id]) 
+        for img_id in semi_indices[task_id]:
+            train_semi_dataset.imgs_list.append(semi_dataset.imgs_list[img_id]) 
+            train_semi_dataset.labels_list.append(semi_dataset.labels_list[img_id]) 
+            train_semi_dataset.masks_list.append(semi_dataset.masks_list[img_id]) 
+            train_semi_dataset.task_ids_list.append(semi_dataset.task_ids_list[img_id]) 
 
     for task_id in range(train_dataset.num_task):
-        noisy_indices = []
-        for i in range(noisy_dataset.sample_num_in_task[task_id]):
-            local_idx = i + len(train_dataset.imgs_list) + sum(noisy_dataset.sample_num_in_task[:task_id])
-            noisy_indices.append(int(local_idx))
+        semi_indices = []
+        for i in range(semi_dataset.sample_num_in_task[task_id]):
+            local_idx = i + len(train_dataset.imgs_list) + sum(semi_dataset.sample_num_in_task[:task_id])
+            semi_indices.append(int(local_idx))
 
-        train_noisy_dataset.sample_indices_in_task[task_id].extend(noisy_indices)
-        train_noisy_dataset.sample_num_in_task[task_id] += noisy_dataset.sample_num_in_task[task_id] 
+        train_semi_dataset.sample_indices_in_task[task_id].extend(semi_indices)
+        train_semi_dataset.sample_num_in_task[task_id] += semi_dataset.sample_num_in_task[task_id] 
 
-    return train_noisy_dataset, valid_noisy_dataset, noisy_dataset
+    return train_semi_dataset, valid_semi_dataset, semi_dataset
