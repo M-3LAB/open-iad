6d23ca078062bc8e7c3606b1112eb1ad4c640158
jinbao-wang <linkingring@163.com>
2023-05-19 17:26:02 +0800

    TODO

--- paradigms/centralized/centralized_learning_3d.py
+++ paradigms/centralized/centralized_learning_3d.py
@@ -1,8 +1,7 @@
-import yaml
-from tools.utils import *
-
 from configuration.device import assign_service
 from rich import print
+from tools.utils import *
+import yaml
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -10,40 +9,5 @@ warnings.filterwarnings("ignore")
 class CentralizedAD3D():
     def __init__(self, args):
         self.args = args
-
-    def load_config(self):
-        with open('./configuration/architecture/3_dataset_base/{}.yaml'.format(self.args.dataset), 'r') as f:
-            config_model = yaml.load(f, Loader=yaml.SafeLoader)
-        with open('./configuration/architecture/2_train_base/centralized_learning.yaml', 'r') as f:
-            config_train = yaml.load(f, Loader=yaml.SafeLoader)
-        with open('./configuration/architecture/1_model_base/{}.yaml'.format(self.args.model), 'r') as f:
-            config_dataset = yaml.load(f, Loader=yaml.SafeLoader)
-
-        config = override_config(config_model, config_train)
-        config = override_config(config, config_dataset)
-        self.para_dict = merge_config(config, self.args)
-        self.args = extract_config(self.args)
-
-        ip, root_path = assign_service(self.para_dict['guoyang'])
-        print('local ip: {}, root_path: {}'.format(ip, root_path))
-
-        self.para_dict['root_path'] = root_path
-        self.para_dict['data_path'] = '{}{}'.format(root_path, self.para_dict['data_path'])
-
-        if not (self.para_dict['vanilla'] or self.para_dict['semi'] or self.para_dict['fewshot'] or self.para_dict['noisy'] or self.para_dict['continual']):
-            raise ValueError('Please Assign Learning Paradigm, --vanilla, --semi, --noisy, --fewshot, --continual')
     
-    def run_work_flow(self):
-        self.load_config()
-        # self.preliminary()
-        # self.load_data()
-        # self.init_model()
-        # print('---------------------')
-
-        # self.work_flow()
-            
-        print('work dir: {}'.format(self.file_path))
-        with open('{}/log_finished.txt'.format(self.para_dict['work_dir']), 'a') as f:
-            print('\n---> work dir {}'.format(self.file_path), file=f)
-            print(self.args, file=f)
-        print('---------------------')
+    # TODO
