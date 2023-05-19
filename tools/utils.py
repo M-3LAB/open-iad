2627edb90b92ecaba84ac9f0c42a7a10284d9960
jinbao-wang <linkingring@163.com>
2023-05-19 16:53:01 +0800

    rm non-usage function

--- tools/utils.py
+++ tools/utils.py
@@ -1,29 +1,17 @@
 import os
-from PIL import Image
 import torch
 import random
 import numpy as np
 import torch
-import numpy as np
 import random
 import yaml
 import time
 import shutil
 import torchvision
-import glob
-import pickle
 
-__all__ = ['seed_everything', 'parse_device_list', 'allocate_gpus', 
-           'merge_config', 'convert_list_float_type', 'draem_weights_init', 'load_model', 
-           'override_config', 'extract_config', 'create_folders', 'save_feat_pickle', 'load_feat_pickle',
-           'record_path', 'save_arg', 'save_log', 'save_script', 'save_image', 'save_model', 'ck_name_parsing']
+__all__ = ['seed_everything', 'parse_device_list', 'merge_config', 'override_config', 'extract_config', 'create_folders', 
+           'record_path', 'save_arg', 'save_log', 'save_script', 'save_image']
 
-def set_grad(model, flag=True):
-    for p in model.parameters():
-        p.requires_grad = flag
-
-def convert_list_float_type(l):
-    return [float(item) for item in l]
 
 def seed_everything(seed):
     random.seed(seed)
@@ -36,13 +24,6 @@ def seed_everything(seed):
     torch.backends.cudnn.deterministic = True
 
 
-def parse_client_data_weights(l):
-    for i in range(len(l)):
-        l[i] = float(l[i])
-        assert isinstance(l[i], float)
-    return l
-
-
 def parse_device_list(device_ids_string, id_choice=None):
     device_ids = [int(i) for i in device_ids_string[0]]
     id_choice = 0 if id_choice is None else id_choice
@@ -50,21 +31,13 @@ def parse_device_list(device_ids_string, id_choice=None):
     return device, device_ids
 
 
-def allocate_gpus(id, num_disc, num_gpus):
-    partitions = np.linspace(0, 1, num_gpus, endpoint=False)[1:]
-    device_id = 0
-    for p in partitions:
-        if id <= num_disc * p:
-            break
-        device_id += 1
-    return device_id
-
 def override_config(previous, new):
     config = previous
     for new_key in new.keys():
             config[new_key] = new[new_key]
 
     return config
+
 
 def merge_config(config, args):
     """
@@ -84,13 +57,6 @@ def extract_config(args):
 
     return config
 
-def draem_weights_init(m):
-    classname = m.__class__.__name__
-    if classname.find("Conv") != -1:
-        torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
-    elif classname.find("BatchNorm2d") != -1:
-        torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
-        torch.nn.init.constant_(m.bias.data, 0.0)
 
 def record_path(para_dict):
     # mkdir ./work_dir/fed/brats/time-dir
@@ -127,46 +93,9 @@ def save_image(image, name, image_path):
     torchvision.utils.save_image(image, '{}/{}'.format(image_path, name), normalize=False)
 
 
-def save_model(model, file_path, infor, save_previous=False):
-    if not os.path.exists(file_path):
-        os.makedirs(file_path)
-
-    if not save_previous:
-        for file in glob.glob('{}/*.pth'.format(file_path)):
-            os.remove(file)      
-
-    model_path = '{}/{}.pth'.format(file_path, infor)
-    torch.save({'model_state_dict': model.state_dict()}, model_path)
-
-def load_model(model, file_path, description):
-    if not os.path.exists(file_path):
-        raise ValueError('file is not exist, {}'.format(file_path)) 
-
-    #model_path = glob.glob('{}/checkpoint/{}/*.pth'.format(file_path, description))[0]
-    model_path = f'{file_path}/{description}.pth' 
-    checkpoint = torch.load(model_path, map_location='cpu')
-    model.load_state_dict(checkpoint['model_state_dict'])
-
-    return model
-
 def create_folders(tag_path):
     if not os.path.exists(tag_path):
         os.makedirs(tag_path)
-
-def save_feat_pickle(feat, file_path):
-    with open(file_path, 'wb') as f:
-        pickle.dump(feat, f)
-
-def load_feat_pickle(file_path):
-    with open(file_path, 'rb') as f:
-        feat = pickle.load(f)
-        return feat
-
-def ck_name_parsing(name):
-    # name: string
-    name_splits = name.split("_") 
-    return name_splits
-
 
 
 
